a77442cb7c2e1cca3b6f2b622a3d63a94da8fb21
dayj1 <Jordan.Day@ons.gov.uk>
2024-04-25 16:12:17 +0100

    Changed input to forward or backward flag and default value for time difference

--- src/flag_and_count_matched_pairs.py
+++ src/flag_and_count_matched_pairs.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np 
 
-
 #df['id','stratum','question']
 # Col names as parameters - pass as list
-def flag_matched_pair_merge(df,delta_t):#,ref_col,period_col,target_col,stratum_col):
+#,ref_col,period_col,target_col,stratum_col):
+
+def flag_matched_pair_merge(df, forward_or_backward, time_difference=1):
     """
     function to add flag to df if data forms a matched pair
     i.e. data is given for both period and predictive period
@@ -12,36 +13,52 @@ def flag_matched_pair_merge(df,delta_t):#,ref_col,period_col,target_col,stratum_
     ----------
     df : pd.DataFrame
         pandas dataframe of original data
-    delta_t : int
+    forward_or_backward: str
+        either f or b for forward or backward method
+    time_difference : int
         time difference between predictive and target period
-         Should this be a more meaningful name? can work forwards or backwards
+        Further refinement on name and details - 
+        default of 1, what does this mean, 1 day, month, hour??
+        Should this be a more meaningful name? can work forwards or backwards
+
 
     Returns
     -------
     pd.DataFrame
-        dataframe with column added added for flagging forward matched paris
+        dataframe with column added flagging forward matched paris
     """    
 
+    if forward_or_backward == 'f':
+        time_difference = time_difference
+    elif forward_or_backward == 'b':
+        time_difference =  -time_difference
 
     df_with_predictive_column = df[["reference", "stratum", "target_variable"]]
-    df_with_predictive_column["predictive_period"] = df["period"] + delta_t
+    df_with_predictive_column["predictive_period"] = df["period"] + time_difference # Shifting period for forward or backward
     df_with_predictive_column.rename(columns={'target_variable' : 'predictive_target_variable'},inplace = True)
-    # Convert to datetime or write own implementation 
+
     
     df = df.merge(df_with_predictive_column,
                   left_on=["reference", "period", "stratum"],
                   right_on=["reference", "predictive_period", "stratum"],
                   how="left")
-        
-    matched_col_name = str(np.where(delta_t > 0,'f_matched_pair','b_matched_pair'))
-    # should forwards be with positive delta_t or minus?
+    
+    df['predictive_period'] -= time_difference #returning predictive period to correct value after join
+    # Can drop after join, only keeping for easy debugging
+
+    # input should be datetime and not str, this implementation will break
+    matched_col_name = forward_or_backward + '_matched_pair'
 
     df[matched_col_name] = np.where(
         df[['target_variable','predictive_target_variable']].isnull().any(axis=1),
         False, 
         True)
+    
+    # Dropping predictive columns for now, these are needed for link calculation.
+    # If not dropped we get issues if running both forward and backwards count without dropping
+    df.drop(['predictive_target_variable','predictive_period'],axis = 1, inplace=True)
     return df
- 
+
 
 def count_matched_pair(df,matched_col_name):
     """
