c221f5bfb230a62bbf41221295fca749bd5dcffd
hemsir <Rowan.Hemsi@ons.gov.uk>
2024-05-01 19:24:07 +0100

    WIP: Add test for construction match count
    This commit adds a test for construction matches count and updates
    the count_matched_pair function so that it also works for construction
    matches. The refactored function may still work for forward and
    backward matches, but the tests were failing due to a filepath error
    before the refactor so not able to confirm.

--- tests/test_construction_matches.py
+++ tests/test_construction_matches.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from pandas.testing import assert_frame_equal
 
 from src.construction_matches import flag_construction_matches
+from src.flag_and_count_matched_pairs import count_matched_pair
 
 @pytest.fixture(scope="class")
 def construction_test_data():
@@ -13,8 +14,34 @@ def construction_test_data():
     return dataframe
 
 class TestConstructionMatches:
-    def test_construction_matches(self, construction_test_data):
-        expected_output = construction_test_data[["target", "period", "auxiliary", "flag_construction_matches"]]
-        input_data = expected_output.drop(labels=["flag_construction_matches"], axis=1)
+    def test_construction_matches_flag(self, construction_test_data):
+        expected_output = construction_test_data[[
+            "target",
+            "period",
+            "auxiliary",
+            "flag_construction_matches",
+        ]]
+
+        input_data = expected_output.drop(columns=["flag_construction_matches"])
         actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
+
+        assert_frame_equal(actual_output, expected_output)
+
+    def test_construction_matches_count(self, construction_test_data):
+        expected_output = construction_test_data[[
+            "period",
+            "flag_construction_matches",
+            "strata",
+            "count_construction_matches",
+        ]]
+
+        input_data = expected_output.drop(columns=["count_construction_matches"])
+        actual_output = count_matched_pair(
+            input_data,
+            "flag_construction_matches",
+            "period",
+            "strata",
+            "count_construction_matches"
+        )
+
         assert_frame_equal(actual_output, expected_output)
