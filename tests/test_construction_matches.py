70ae850493a5e04dbede59e65320bdd9b7ddc5d7
hemsir <Rowan.Hemsi@ons.gov.uk>
2024-05-01 18:48:51 +0100

    Refactor construction matches test

--- tests/test_construction_matches.py
+++ tests/test_construction_matches.py
@@ -1,23 +1,20 @@
 import pytest
 import pandas as pd
-import numpy as np
 
+from pathlib import Path
 from pandas.testing import assert_frame_equal
 
 from src.construction_matches import flag_construction_matches
 
-def test_construction_matches():
-    expected_output = pd.DataFrame(np.array([
-            [42,pd.to_datetime("202401", format="%Y%m"),10,True],
-            [pd.NA,pd.to_datetime("202401", format="%Y%m"),10,False],
-        ]),
-        columns=["target", "period", "auxiliary", "flag_construction_matches"],
-    )
+@pytest.fixture(scope="class")
+def construction_test_data():
+    dataframe = pd.read_csv(Path("tests")/"construction_matches.csv")
+    dataframe["period"] = pd.to_datetime(dataframe["period"], format="%Y%m")
+    return dataframe
 
-    # cast to python non-nullable bool type rather than pandas nullable boolean type
-    expected_output["flag_construction_matches"] = expected_output["flag_construction_matches"].astype(bool)
-
-    input_data = expected_output.drop(labels=["flag_construction_matches"], axis=1)
-    actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
-
-    assert_frame_equal(actual_output, expected_output)
+class TestConstructionMatches:
+    def test_construction_matches(self, construction_test_data):
+        expected_output = construction_test_data[["target", "period", "auxiliary", "flag_construction_matches"]]
+        input_data = expected_output.drop(labels=["flag_construction_matches"], axis=1)
+        actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
+        assert_frame_equal(actual_output, expected_output)
