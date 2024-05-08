56c65594088b425287506e102dfa460d56b4093d
dayj1 <Jordan.Day@ons.gov.uk>
2024-05-08 16:36:52 +0100

    Auto formatting items in test dir by commit hooks

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
+
 def load_and_format(filename):
     """Load csv as pandas dataframe and cast period column to datetime type"""
     df_loaded = pd.read_csv(filename)
-    df_loaded['period'] = pd.to_datetime(df_loaded['period'], format='%Y%m')
+    df_loaded["period"] = pd.to_datetime(df_loaded["period"], format="%Y%m")
     return df_loaded
--- tests/test_construction_matches.py
+++ tests/test_construction_matches.py
@@ -1,37 +1,45 @@
-import pytest
-
 from pathlib import Path
+
+import pytest
+from helper_functions import load_and_format
 from pandas.testing import assert_frame_equal
 
 from src.construction_matches import flag_construction_matches
 from src.flag_and_count_matched_pairs import count_matches
-from helper_functions import load_and_format
+
 
 @pytest.fixture(scope="class")
 def construction_test_data():
-    return load_and_format(Path("tests")/"construction_matches.csv")
+    return load_and_format(Path("tests") / "construction_matches.csv")
+
 
 class TestConstructionMatches:
     def test_construction_matches_flag(self, construction_test_data):
-        expected_output = construction_test_data[[
-            "target",
-            "period",
-            "auxiliary",
-            "flag_construction_matches",
-        ]]
+        expected_output = construction_test_data[
+            [
+                "target",
+                "period",
+                "auxiliary",
+                "flag_construction_matches",
+            ]
+        ]
 
         input_data = expected_output.drop(columns=["flag_construction_matches"])
-        actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
+        actual_output = flag_construction_matches(
+            input_data, "target", "period", "auxiliary"
+        )
 
         assert_frame_equal(actual_output, expected_output)
 
     def test_construction_matches_count(self, construction_test_data):
-        expected_output = construction_test_data[[
-            "period",
-            "flag_construction_matches",
-            "strata",
-            "count_construction_matches",
-        ]]
+        expected_output = construction_test_data[
+            [
+                "period",
+                "flag_construction_matches",
+                "strata",
+                "count_construction_matches",
+            ]
+        ]
 
         input_data = expected_output.drop(columns=["count_construction_matches"])
         actual_output = count_matches(
@@ -39,7 +47,7 @@ class TestConstructionMatches:
             "flag_construction_matches",
             "period",
             "strata",
-            "count_construction_matches"
+            "count_construction_matches",
         )
 
         assert_frame_equal(actual_output, expected_output)
