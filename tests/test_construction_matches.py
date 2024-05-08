0e30173f61d94d2126531e0b6809de3c8a63be73
Rowan Hemsi_ONS <70013144+rowanhemsi@users.noreply.github.com>
2024-05-08 14:54:44 +0100

    324 construction link count (#9)
    * Refactor construction matches test
    
    * WIP: Add test for construction match count
    
    * Refactor count_matched_pairs and tests
    
    This commit adds a test for construction matches count and refactors
    the count_matched_pair function into count_matches, which works for
    forward, backward and construction matches

--- tests/test_construction_matches.py
+++ tests/test_construction_matches.py
@@ -1,23 +1,45 @@
 import pytest
-import pandas as pd
-import numpy as np
 
+from pathlib import Path
 from pandas.testing import assert_frame_equal
 
 from src.construction_matches import flag_construction_matches
+from src.flag_and_count_matched_pairs import count_matches
+from helper_functions import load_and_format
 
-def test_construction_matches():
-    expected_output = pd.DataFrame(np.array([
-            [42,pd.to_datetime("202401", format="%Y%m"),10,True],
-            [pd.NA,pd.to_datetime("202401", format="%Y%m"),10,False],
-        ]),
-        columns=["target", "period", "auxiliary", "flag_construction_matches"],
-    )
+@pytest.fixture(scope="class")
+def construction_test_data():
+    return load_and_format(Path("tests")/"construction_matches.csv")
 
-    # cast to python non-nullable bool type rather than pandas nullable boolean type
-    expected_output["flag_construction_matches"] = expected_output["flag_construction_matches"].astype(bool)
+class TestConstructionMatches:
+    def test_construction_matches_flag(self, construction_test_data):
+        expected_output = construction_test_data[[
+            "target",
+            "period",
+            "auxiliary",
+            "flag_construction_matches",
+        ]]
 
-    input_data = expected_output.drop(labels=["flag_construction_matches"], axis=1)
-    actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
+        input_data = expected_output.drop(columns=["flag_construction_matches"])
+        actual_output = flag_construction_matches(input_data, "target", "period", "auxiliary")
 
-    assert_frame_equal(actual_output, expected_output)
+        assert_frame_equal(actual_output, expected_output)
+
+    def test_construction_matches_count(self, construction_test_data):
+        expected_output = construction_test_data[[
+            "period",
+            "flag_construction_matches",
+            "strata",
+            "count_construction_matches",
+        ]]
+
+        input_data = expected_output.drop(columns=["count_construction_matches"])
+        actual_output = count_matches(
+            input_data,
+            "flag_construction_matches",
+            "period",
+            "strata",
+            "count_construction_matches"
+        )
+
+        assert_frame_equal(actual_output, expected_output)
--- tests/test_flag_and_count_matched_pairs.py
+++ tests/test_flag_and_count_matched_pairs.py
@@ -1,27 +1,27 @@
 import pandas as pd
 import pytest
-from src.flag_and_count_matched_pairs import flag_matched_pair_merge, count_matched_pair, flag_matched_pair_shift
+
 from pandas.testing import assert_frame_equal
+from pathlib import Path
+
+from src.flag_and_count_matched_pairs import flag_matched_pair_merge, count_matches, flag_matched_pair_shift
+from helper_functions import load_and_format
 
 # Case 1 - two businesses, one missing value
 # Case 2 - change in strata (sic) 
 # Case 3 - Missing period for one business 
 
+filepath = Path('tests')/'test_data_matched_pair'
 
 file_name_cases = [
-    ('tests/case1_expected_output.csv'),
-    ('tests/case2_expected_output.csv'),
-    ('tests/case3_expected_output.csv')
+    (filepath/'case1_expected_output.csv'),
+    (filepath/'case2_expected_output.csv'),
+    (filepath/'case3_expected_output.csv'),
     ]
-
-def load_and_format(filename):
-    df_loaded = pd.read_csv(filename)
-    df_loaded['period'] = pd.to_datetime(df_loaded['period'], format='%Y%m')
-    return df_loaded
 
 pytestmark = pytest.mark.parametrize("expected_output_file",file_name_cases)
 
-class TestClass:
+class TestMatchedPair:
     def test_flag_matched_pair_merge_forward(self, expected_output_file):
         df_expected_output = load_and_format(expected_output_file)
         df_expected_output.drop(['f_matched_pair_count','b_matched_pair','b_matched_pair_count'],axis = 1,inplace=True)
@@ -42,14 +42,14 @@ class TestClass:
         df_expected_output = load_and_format(expected_output_file)
         df_expected_output.drop(['b_matched_pair','b_matched_pair_count'],axis = 1,inplace=True)
         df_input = df_expected_output[['reference', 'strata', 'period', 'target_variable', 'f_matched_pair']]
-        df_output = count_matched_pair(df_input,'f_matched_pair','period','strata')
+        df_output = count_matches(df_input,'f_matched_pair','period','strata')
         assert_frame_equal(df_output, df_expected_output)
 
-    def test_count_matched_pair_backward(self, expected_output_file):
+    def test_count_matches_backward(self, expected_output_file):
         df_expected_output = load_and_format(expected_output_file)
         df_expected_output.drop(['f_matched_pair','f_matched_pair_count'],axis = 1,inplace=True)
         df_input = df_expected_output[['reference', 'strata', 'period', 'target_variable', 'b_matched_pair']]
-        df_output = count_matched_pair(df_input,'b_matched_pair','period','strata')
+        df_output = count_matches(df_input,'b_matched_pair','period','strata')
         assert_frame_equal(df_output, df_expected_output)
 
     def test_flag_matched_pair_shift_forward(self, expected_output_file):
