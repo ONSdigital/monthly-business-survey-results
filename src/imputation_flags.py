f33414756ca861ee2133600fa0b87c2abcd41091
dayj1 <Jordan.Day@ons.gov.uk>
2024-05-16 11:12:22 +0100

    Adding docstrings

--- src/imputation_flags.py
+++ src/imputation_flags.py
@@ -2,8 +2,34 @@ import numpy as np
 
 
 def create_impute_flags(df, target, reference, strata, auxiliary):
-    temp_df = df.copy()
-    df["f_predictive_" + target + "_roll"] = temp_df.groupby([reference, strata])[
+    """
+    function to create logical columns for each type of imputation
+    Will feed into creating a column for imputation method
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing forward, backward predictive period columns (
+        These columns are created by calling flag_matched_pair_merge forward
+        and backwards)
+
+    target : str
+        column name containing target variable
+    reference : str
+        column name containing business reference id
+    strata : str
+        column name containing strata information (sic)
+    auxiliary : str
+        column name containing auxiliary data
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with four additional logical columns determining if target
+        is a return (r_flag) can be imputed by forward imputation (fir_flag),
+        backward imputation (bir_flag) or can be constructed (c_flag)
+    """
+    df["f_predictive_" + target + "_roll"] = df.groupby([reference, strata])[
         "f_predictive_" + target
     ].ffill()
     df["b_predictive_" + target + "_roll"] = df.groupby([reference, strata])[
