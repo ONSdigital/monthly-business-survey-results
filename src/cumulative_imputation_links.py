9fd0604989165c2b6810ab44bfd5204a1bc7ab2c
roberw <Wil.Roberts@ons.gov.uk>
2024-05-16 15:30:05 +0100

    adding pre-commit hooks

--- src/cumulative_imputation_links.py
+++ src/cumulative_imputation_links.py
@@ -1,58 +1,65 @@
-import pandas as pd
 import numpy as np
 
 
-def get_cumulative_links(dataframe, forward_or_backward, time_difference, strata, reference, target, period, imputation_link):
+def get_cumulative_links(
+    dataframe,
+    forward_or_backward,
+    time_difference,
+    strata,
+    reference,
+    target,
+    period,
+    imputation_link,
+):
     """
-    Create cumulative imputation links for multiple consecutive periods without a return.
+    Create cumulative imputation links for multiple consecutive periods
+    without a return.
 
     Parameters
     ----------
     dataframe : pandas.DataFrame
     forward_or_backward: str
         either f or b for forward or backward method
+    time_difference : int
+        time difference between predictive and target period in months
+    strata : str
+        column name containing strata information (sic)
+    reference : str
+        column name containing business reference id
     target : str
         column name containing target variable
     period : str
         column name containing time period
-    reference : str
-        column name containing business reference id
-    strata : str
-        column name containing strata information (sic)
-    time_difference : int
-        time difference between predictive and target period in months
     imputation_link : string
         column name containing imputation links
 
     Returns
     -------
     pandas.DataFrame
-        dataframe with additional missing_value, imputation group and 
-        cumulative_imputation_link column
+        dataframe with imputation_group and
+        cumulative_forward/backward_imputation_link column
     """
-    
-    dataframe.sort_values([strata, reference, period], inplace=True)    
+
+    dataframe.sort_values([strata, reference, period], inplace=True)
     dataframe["missing_value"] = np.where(dataframe[target].isnull(), True, False)
-    
+
     dataframe["imputation_group"] = (
-        ((dataframe["missing_value"].diff(time_difference) != 0) |
-        (dataframe[strata].diff(time_difference) != 0) |
-        (dataframe[reference].diff(time_difference) != 0))
-    .astype("int")
-    .cumsum()
-    )
-    
-    if forward_or_backward == "f":
-        dataframe["cumulative_"+imputation_link] = (
-            dataframe.groupby("imputation_group")[imputation_link]
-            .cumprod()
+        (
+            (dataframe["missing_value"].diff(time_difference) != 0)
+            | (dataframe[strata].diff(time_difference) != 0)
+            | (dataframe[reference].diff(time_difference) != 0)
         )
+        .astype("int")
+        .cumsum()
+    )
+
+    if forward_or_backward == "f":
+        dataframe["cumulative_" + imputation_link] = dataframe.groupby(
+            "imputation_group"
+        )[imputation_link].cumprod()
     elif forward_or_backward == "b":
-        dataframe["cumulative_"+imputation_link] = (
-            dataframe[::-1].groupby("imputation_group")[imputation_link]
-            .cumprod()[::-1]
-        )        
-    
-    return dataframe[["imputation_group", "cumulative_"+imputation_link]]
+        dataframe["cumulative_" + imputation_link] = (
+            dataframe[::-1].groupby("imputation_group")[imputation_link].cumprod()[::-1]
+        )
 
-
+    return dataframe[["imputation_group", "cumulative_" + imputation_link]]
--- tests/test_cumulative_imputation_links.py
+++ tests/test_cumulative_imputation_links.py
@@ -6,6 +6,7 @@ from pandas.testing import assert_frame_equal
 
 from src.cumulative_imputation_links import get_cumulative_links
 
+
 @pytest.fixture(scope="class")
 def cumulative_links_test_data():
     return load_and_format(Path("tests") / "cumulative_links.csv")
@@ -13,12 +14,10 @@ def cumulative_links_test_data():
 
 class TestComulativeLinks:
     def test_get_cumulative_links_forward(self, cumulative_links_test_data):
-        input_data = (
-            cumulative_links_test_data.drop(
-                columns=["cumulative_forward_imputation_link",
-                         "imputation_group"])
+        input_data = cumulative_links_test_data.drop(
+            columns=["cumulative_forward_imputation_link", "imputation_group"]
         )
-        
+
         expected_output = cumulative_links_test_data[
             [
                 "imputation_group",
@@ -26,22 +25,24 @@ class TestComulativeLinks:
             ]
         ]
 
-
         actual_output = get_cumulative_links(
-            input_data, "f", 1, "strata", "reference", "target", "period",
-            "forward_imputation_link"
+            input_data,
+            "f",
+            1,
+            "strata",
+            "reference",
+            "target",
+            "period",
+            "forward_imputation_link",
         )
 
         assert_frame_equal(actual_output, expected_output)
 
-    
     def test_get_cumulative_links_backward(self, cumulative_links_test_data):
-        input_data = (
-            cumulative_links_test_data.drop(
-                columns=["cumulative_backward_imputation_link",
-                         "imputation_group"])
+        input_data = cumulative_links_test_data.drop(
+            columns=["cumulative_backward_imputation_link", "imputation_group"]
         )
-        
+
         expected_output = cumulative_links_test_data[
             [
                 "imputation_group",
@@ -49,10 +50,15 @@ class TestComulativeLinks:
             ]
         ]
 
-
         actual_output = get_cumulative_links(
-            input_data, "b", 1, "strata", "reference", "target", "period",   
-            "backward_imputation_link"
+            input_data,
+            "b",
+            1,
+            "strata",
+            "reference",
+            "target",
+            "period",
+            "backward_imputation_link",
         )
 
         assert_frame_equal(actual_output, expected_output)
