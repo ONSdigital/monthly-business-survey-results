3928f44a24498ff3878656ece2bc038c1c484e0d
roberw <Wil.Roberts@ons.gov.uk>
2024-05-22 09:56:40 +0100

    reading data

--- playground/read_snapshot.py
+++ playground/read_snapshot.py
@@ -1,11 +1,12 @@
 import json
-import pandas as pd
 import os
+
+import pandas as pd
 
 data_folder = os.getcwd() + "/monthly-business-survey-results/playground/data/"
 
-f = open(data_folder + 'snapshot-202212-002-2156d36b-e61f-42f1-a0f1-61d1f8568b8e.json')
- 
+f = open(data_folder + "snapshot-202212-002-2156d36b-e61f-42f1-a0f1-61d1f8568b8e.json")
+
 data = json.load(f)
 
 data.keys()
@@ -18,3 +19,8 @@ responses = pd.DataFrame(data["responses"])
 
 contributors.T
 responses
+
+
+df = pd.read_json(
+    data_folder + "snapshot-202212-002-2156d36b-e61f-42f1-a0f1-61d1f8568b8e.json"
+)
