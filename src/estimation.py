6ce853a17d9a7ee8fc2655ff47ad2894f46ade96
hemsir <Rowan.Hemsi@ons.gov.uk>
2024-06-06 18:48:55 +0100

    Add calibration weight calculation

--- src/estimation.py
+++ src/estimation.py
@@ -1,8 +1,14 @@
+from typing import Optional
+
 import pandas as pd
 
 
 def calculate_a_weight(
-    dataframe: pd.DataFrame, period: str, strata: str, sampled: str
+    dataframe: pd.DataFrame,
+    period: str,
+    group: str,
+    sampled: str,
+    a_weight: Optional[str] = "a_weight",
 ) -> pd.DataFrame:
     """
     Add column to dataframe containing a weights based on sampled flag
@@ -13,31 +19,92 @@ def calculate_a_weight(
         data to be estimated
     period : str
         name of column in dataframe containing period variable
-    strata : str
-        name of column in dataframe containing strata variable
+    group : str
+        name of column in dataframe containing group variable
+        this should usually be the strata variable
     sampled : str
         name of column in dataframe containing sample flag with values 0 or 1
+    a_weight : str
+        name to be given to new column containing a_weights
+        defaults to `a_weights`
 
     Returns
     -------
     pd.DataFrame
-        dataframe with new column `a_weight`
+        dataframe with new column containing a_weights
 
     Notes
     -----
     Assumes all strata are sampled - is this a valid assumption?
     #TODO: Add link to specification
     """
-    population_counts = dataframe.groupby([period, strata]).size()
+    population_counts = dataframe.groupby([period, group]).size()
 
     sample = dataframe[dataframe[sampled] == 1]
-    sample_counts = sample.groupby([period, strata]).size()
+    sample_counts = sample.groupby([period, group]).size()
 
     a_weights = population_counts / sample_counts
 
-    a_weights.name = "a_weight"
+    a_weights.name = a_weight
     a_weights = a_weights.reset_index()
 
-    dataframe = dataframe.merge(a_weights, how="left", on=[period, strata])
+    dataframe = dataframe.merge(a_weights, how="left", on=[period, group])
+
+    return dataframe
+
+
+def calculate_calibration_factor(
+    dataframe: pd.DataFrame,
+    period: str,
+    group: str,
+    sampled: str,
+    auxiliary: str,
+) -> pd.DataFrame:
+    """
+     Add column to dataframe to calculate calibration factor
+
+     Parameters
+     ----------
+     dataframe : pd.DataFrame
+         data to be weighted
+     period : str
+         name of column in dataframe containing period variable
+    group: str
+         name of column in dataframe containing group level data
+         for separate ratio, use strata variable
+         for combined ratio, use calibration group level variable
+     sampled : str
+         name of column in dataframe containing sample flag
+     auxiliary : str
+         name of column in dataframe containing auxiliary variable
+     design_weight: str
+         name of column in dataframe containing design weight
+
+     Returns
+     -------
+     pd.DataFrame
+         dataframe with new column `calibration_factor`
+    """
+    # design weights used in calibration factor calculation should be based on
+    # group (not necessarily strata) and should not overwrite existing weights
+    dataframe_copy = dataframe.copy()
+    dataframe_copy = calculate_a_weight(
+        dataframe_copy, period, group, sampled, "group_design_weight"
+    )
+
+    population_sums = dataframe_copy.groupby([period, group])[auxiliary].sum()
+
+    # copy again to avoid SettingWithCopy warning
+    # (not required with later versions of pandas)
+    sample = dataframe_copy.copy()[dataframe_copy[sampled] == 1]
+    sample["weighted_auxiliary"] = sample[auxiliary] * sample["group_design_weight"]
+    weighted_sample_sums = sample.groupby([period, group])["weighted_auxiliary"].sum()
+
+    calibration_factor = population_sums / weighted_sample_sums
+
+    calibration_factor.name = "calibration_factor"
+    calibration_factor = calibration_factor.reset_index()
+
+    dataframe = dataframe.merge(calibration_factor, how="left", on=[period, group])
 
     return dataframe
--- tests/test_estimation.py
+++ tests/test_estimation.py
@@ -2,19 +2,19 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
-from src.estimation import calculate_a_weight
+from src.estimation import calculate_a_weight, calculate_calibration_factor
 
 
 @pytest.fixture(scope="class")
-def a_weight_data():
-    return pd.read_csv(Path("tests") / "data" / "estimation_a_weights.csv")
+def filepath():
+    return Path("tests") / "data" / "estimation"
 
 
 class TestEstimation:
-    def test_calculate_a_weights(self, a_weight_data):
-        expected_output = a_weight_data
+    def test_calculate_a_weights(self, filepath):
+        expected_output = pd.read_csv(filepath / "a_weights.csv")
 
         input_data = expected_output.drop(columns=["a_weight"])
         actual_output = calculate_a_weight(
@@ -25,3 +25,25 @@ class TestEstimation:
         )
 
         assert_frame_equal(actual_output, expected_output)
+
+    @pytest.mark.parametrize(
+        "csv,group",
+        [
+            ("calibration_factor_separate.csv", "strata"),
+            ("calibration_factor_combined.csv", "calibration_group"),
+        ],
+    )
+    def test_calculate_calibration_factor(self, filepath, csv, group):
+        expected_output = pd.read_csv(filepath / csv)
+
+        input_data = expected_output.drop(columns=["calibration_factor"])
+        actual_output = calculate_calibration_factor(
+            input_data,
+            "period",
+            group,
+            "sampled",
+            "auxiliary",
+        )
+
+        assert_frame_equal(actual_output, expected_output)
+        assert_series_equal(expected_output["a_weight"], actual_output["a_weight"])
