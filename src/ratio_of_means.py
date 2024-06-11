c5a24efcc9f1735f4361b41777a3ed88d254de67
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-06-11 17:45:01 +0100

    Wrap calculate_imputation_link function

--- src/ratio_of_means.py
+++ src/ratio_of_means.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from construction_matches import flag_construction_matches
 from flag_and_count_matched_pairs import count_matches, flag_matched_pair_merge
+from forward_link import calculate_imputation_link
 
 
 def flag_all_match_pairs(
@@ -80,5 +81,56 @@ def count_all_matches(
 
     for args in count_arguments:
         df = count_matches(df, **args)
+
+    return df
+
+
+def calculate_all_links(
+    df: pd.DataFrame, **default_columns: Dict[str, str]
+) -> pd.DataFrame:
+    """
+    Creates 3 new numeric columns with the link between target and
+    predictive_variable for every imputation method. This is wrapper for
+    get_cumulative_links function.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Original dataframe.
+    **default_columns : Dict[str, str]
+        The column names kwargs which were passed to ratio of means function.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Original dataframe with 3 new numeric columns.
+    """
+
+    link_arguments = (
+        dict(
+            **default_columns,
+            **{
+                "match_col": "f_matched_pair_question",
+                "predictive_variable": "f_predictive_question",
+            }
+        ),
+        dict(
+            **default_columns,
+            **{
+                "match_col": "b_matched_pair_question",
+                "predictive_variable": "b_predictive_question",
+            }
+        ),
+        {
+            "period": "date",
+            "strata": "group",
+            "match_col": "flag_construction_matches",
+            "target": "question",
+            "predictive_variable": "other",
+        },
+    )
+
+    for args in link_arguments:
+        df = calculate_imputation_link(df, **args)
 
     return df
