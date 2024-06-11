4b7f02e0293cb0343fbb5dedb912d411e95db3e7
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-06-11 17:43:42 +0100

    Wrap count matches function

--- src/ratio_of_means.py
+++ src/ratio_of_means.py
@@ -3,7 +3,7 @@ from typing import Dict
 import pandas as pd
 
 from construction_matches import flag_construction_matches
-from flag_and_count_matched_pairs import flag_matched_pair_merge
+from flag_and_count_matched_pairs import count_matches, flag_matched_pair_merge
 
 
 def flag_all_match_pairs(
@@ -47,5 +47,38 @@ def flag_all_match_pairs(
         df = flag_matched_pair_merge(df, **args)
 
     df = flag_construction_matches(df, **default_columns)
+
+    return df
+
+
+def count_all_matches(
+    df: pd.DataFrame, **default_columns: Dict[str, str]
+) -> pd.DataFrame:
+    """
+    Creates 3 new numeric columns with the sum of matches between targeted and
+    predicted values for each imputation method. This is wrapper for
+    count_matches function.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Original dataframe.
+    **default_columns : Dict[str, str]
+        The column names kwargs which were passed to ratio of means function.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Original dataframe with 3 new numeric columns.
+    """
+
+    count_arguments = (
+        dict(**default_columns, **{"flag_column_name": "f_matched_pair_question"}),
+        dict(**default_columns, **{"flag_column_name": "b_matched_pair_question"}),
+        dict(**default_columns, **{"flag_column_name": "flag_construction_matches"}),
+    )
+
+    for args in count_arguments:
+        df = count_matches(df, **args)
 
     return df
