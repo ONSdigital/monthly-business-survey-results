60d7f3a20a3383fcfc42c8c17dcc6cb6fb4f7691
hemsir <Rowan.Hemsi@ons.gov.uk>
2024-06-12 11:32:11 +0100

    Refactor imputation link to work for construction

--- src/calculate_imputation_link.py
+++ src/calculate_imputation_link.py
@@ -9,6 +9,7 @@ def calculate_imputation_link(
     match_col: str,
     target_variable: str,
     predictive_variable: str,
+    link_col: str,
 ) -> pd.DataFrame:
     """
     Calculate link between target_variable and predictive_variable by strata,
@@ -29,29 +30,16 @@ def calculate_imputation_link(
         Column name of the targeted variable.
     predictive_variable : str
         Column name of the predicted target variable.
+    link_col : str
+        Name to use for the new column containing imputation link
 
     Returns
     -------
     df : pd.DataFrame
-        A pandas DataFrame with a new column containing either f_link or b_link
-        based on the input parameters.
+        A pandas DataFrame with a new column containing imputation link.
     """
 
     df_intermediate = df.copy()
-
-    if match_col == "f_matched_pair" and predictive_variable == "f_predictive_question":
-        link_col_name = "f_link"
-
-    elif (
-        match_col == "b_matched_pair" and predictive_variable == "b_predictive_question"
-    ):
-        link_col_name = "b_link"
-
-    else:
-        raise ValueError(
-            f"""
-        {match_col} and {predictive_variable} do not have same wildcard."""
-        )
 
     df_intermediate[target_variable] = (
         df_intermediate[target_variable] * df_intermediate[match_col]
@@ -71,6 +59,6 @@ def calculate_imputation_link(
 
     denominator.replace(0, np.nan, inplace=True)  # cover division with 0
 
-    df[link_col_name] = numerator / denominator
+    df[link_col] = numerator / denominator
 
     return df
