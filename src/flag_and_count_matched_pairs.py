93f6eecef678213f4779b6f4f7586f7ac6fbe713
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-06-20 11:24:13 +0100

    Add kwargs for mapping

--- src/flag_and_count_matched_pairs.py
+++ src/flag_and_count_matched_pairs.py
@@ -2,9 +2,7 @@ import numpy as np  # noqa F401
 import pandas as pd  # noqa F401
 
 
-<<<<<<< HEAD
-
-def flag_matched_pair_merge(
+def flag_matched_pair(
     df,
     forward_or_backward,
     target,
@@ -74,61 +72,7 @@ def flag_matched_pair_merge(
     return df
 
 
-def flag_matched_pair_shift(
-    df, forward_or_backward, target, period, reference, strata, shift=1
-):
-    """
-    function to flag matched pairs using the shift method
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        pandas dataframe of original data
-    forward_or_backward : str
-        number of rows to shift up or down
-    target : str
-        column name containing target variable
-    period : str
-        column name containing time period
-    reference : str
-        column name containing business reference id
-    strata : str
-        column name containing strata information (sic)
-    time_difference: int
-        lookup distance for matched pairs
-
-    Returns
-    -------
-    _type_
-        two pandas dataframes: the main dataframe with column added flagging
-        forward matched pairs and
-        predictive target variable data column
-    """
-
-    df = df.sort_values(by=[reference, period])
-
-    if forward_or_backward == "b":
-        time_difference = -time_difference
-
-    df[forward_or_backward + "_match"] = (
-        df.groupby([strata, reference])
-        .shift(time_difference)[target]
-        .notnull()
-        .mul(df[target].notnull())
-        .mul(
-            df[period] - pd.DateOffset(months=time_difference)
-            == df.shift(time_difference)[period]
-        )
-    )
-
-    df.reset_index(drop=True, inplace=True)
-
-    return df
-
-
-def count_matches(
-    df, flag_column_name, period, strata, count_column_name=None, **kwargs
-):
+def count_matches(df, flag, period, strata, **kwargs):
     """
     function to flag matched pairs using the shift method
 
@@ -144,6 +88,8 @@ def count_matches(
         column name containing time period
     strata : str
         column name containing strata information (sic)
+    kwargs : mapping, optional
+        A dictionary of keyword arguments passed into func.
 
     Returns
     -------
