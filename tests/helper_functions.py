66ac78c11ebfeed0f425d83f9966f400bec122e4
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-06-24 14:37:20 +0100

    Add filter reader function

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 
@@ -6,3 +8,13 @@ def load_and_format(filename):
     df_loaded = pd.read_csv(filename)
     df_loaded["period"] = pd.to_datetime(df_loaded["period"], format="%Y%m")
     return df_loaded
+
+
+def load_filter(filter_path):
+    """Check if path exists and load csv as pandas dataframe"""
+    my_file = Path(filter_path)
+
+    if my_file.is_file():
+
+        df = pd.read_csv(my_file)
+        return df
