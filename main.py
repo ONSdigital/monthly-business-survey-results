ed02d2666187848025a912ca3f96dad1011bd8f4
shilohd <57630783+shilohd@users.noreply.github.com>
2024-06-26 14:11:04 +0100

    Validate input datasets (#39)

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from src.utils.hdfs_mods import hdfs_load_json as read_json
 
 from mbs_results.data_cleaning import clean_and_merge
 from mbs_results.inputs import load_config
+from mbs_results.validation_checks import validate_config
 
 # os.chdir("/home/cdsw/monthly-business-survey-results/")
 
 config = load_config()
+validate_config(config)
+
 snapshot = read_json(config["mbs_results_path"])
 
 clean_and_merge(snapshot=snapshot, **config)
