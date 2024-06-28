694e7c870b92b0888673417a8600613e0eae107a
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-06-28 17:42:38 +0100

    Rename target_variable arg to target

--- mbs_results/calculate_imputation_link.py
+++ mbs_results/calculate_imputation_link.py
@@ -7,15 +7,14 @@ def calculate_imputation_link(
     period: str,
     strata: str,
     match_col: str,
-    target_variable: str,
+    target: str,
     predictive_variable: str,
     link_col: str,
     **kwargs
-    link_col: str,
 ) -> pd.DataFrame:
     """
-    Calculate link between target_variable and predictive_variable by strata,
-    a match_col must be supplied which indicates if target_variable
+    Calculate link between target and predictive_variable by strata,
+    a match_col must be supplied which indicates if target
     and predictive_variable can be linked.
 
     Parameters
@@ -28,7 +27,7 @@ def calculate_imputation_link(
         Column name containing strata information (sic).
     match_col : str
         Column name of the matched pair links, this column should be bool.
-    target_variable : str
+    target : str
         Column name of the targeted variable.
     predictive_variable : str
         Column name of the predicted target variable.
@@ -36,29 +35,22 @@ def calculate_imputation_link(
         Name to use for the new column containing imputation link
     kwargs : mapping, optional
         A dictionary of keyword arguments passed into func.
-    link_col : str
-        Name to use for the new column containing imputation link
 
     Returns
     -------
     df : pd.DataFrame
         A pandas DataFrame with a new column containing imputation link.
-        A pandas DataFrame with a new column containing imputation link.
     """
 
     df_intermediate = df.copy()
 
-    df_intermediate[target_variable] = (
-        df_intermediate[target_variable] * df_intermediate[match_col]
-    )
+    df_intermediate[target] = df_intermediate[target] * df_intermediate[match_col]
 
     df_intermediate[predictive_variable] = (
         df_intermediate[predictive_variable] * df_intermediate[match_col]
     )
 
-    numerator = df_intermediate.groupby([strata, period])[target_variable].transform(
-        "sum"
-    )
+    numerator = df_intermediate.groupby([strata, period])[target].transform("sum")
 
     denominator = df_intermediate.groupby([strata, period])[
         predictive_variable
