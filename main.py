951cc803ddf6174d7c550184b4f2e45ef68e8f37
Jordan Day <jordan.day@ons.gov.uk>
2024-07-11 15:16:43 +0100

    refixing main.py?

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os  # noqa F401
 
+from src.utils.hdfs_mods import hdfs_load_json as read_json
+
 from mbs_results.data_cleaning import (
     clean_and_merge,
     enforce_datatypes,
@@ -7,7 +9,6 @@ from mbs_results.data_cleaning import (
 )
 from mbs_results.inputs import load_config
 from mbs_results.validation_checks import validate_config
-from src.utils.hdfs_mods import hdfs_load_json as read_json
 
 # os.chdir("/home/cdsw/monthly-business-survey-results/")
 
