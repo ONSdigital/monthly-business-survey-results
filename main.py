7acb112e9e7503a33b9514322c39ea168df25bd5
shilod <Daniel.Shiloh@ons.gov.uk>
2024-07-12 14:23:34 +0100

    bugfix: rolling imputation flag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os  # noqa F401
 
+import pandas as pd
 from src.utils.hdfs_mods import hdfs_load_json as read_json
 
 from mbs_results.data_cleaning import (
@@ -7,7 +8,10 @@ from mbs_results.data_cleaning import (
     enforce_datatypes,
     load_manual_constructions,
 )
+from mbs_results.flag_and_count_matched_pairs import flag_matched_pair
+from mbs_results.imputation_flags import create_impute_flags, generate_imputation_marker
 from mbs_results.inputs import load_config
+from mbs_results.utils import convert_column_to_datetime
 from mbs_results.validation_checks import validate_config
 
 # os.chdir("/home/cdsw/monthly-business-survey-results/")
@@ -19,3 +23,21 @@ snapshot = read_json(config["mbs_results_path"])
 df = clean_and_merge(snapshot=snapshot, **config)
 df = enforce_datatypes(df, **config)
 df = load_manual_constructions(df, **config)
+
+df = pd.read_csv("tests/test_data_matched_pair/flag_pairs_2_groups_expected_output.csv")
+df.rename(columns={"strata": "group", "target_variable": "return"}, inplace=True)
+df = df.iloc[:, 0:4]
+df.period = convert_column_to_datetime(df.period)
+df["auxiliary"] = 10
+
+# Simulate manual constructions
+df["return_man"] = [100, None, None, None, 18, 27, None, None]
+
+df = flag_matched_pair(df, forward_or_backward="f", **config)
+df = flag_matched_pair(df, forward_or_backward="b", **config)
+df = flag_matched_pair(
+    df, forward_or_backward="f", **{**config, **{"target": "auxiliary"}}
+)
+
+df = create_impute_flags(df, **config)
+df = generate_imputation_marker(df, config["target"])
--- mbs_results/imputation_flags.py
+++ mbs_results/imputation_flags.py
@@ -11,7 +11,7 @@ def create_impute_flags(
     auxiliary: str,
     predictive_auxiliary: str,
     time_difference=1,
-    **kwargs
+    **kwargs,
 ):
     """
     function to create logical columns for each type of imputation
@@ -52,18 +52,57 @@ def create_impute_flags(
 
     df.sort_values([reference, strata, period], inplace=True)
 
-    df["r_flag"] = df[target].notna()
+    df[f"r_flag_{target}"] = df[target].notna()
 
-    df["fir_flag"] = flag_rolling_impute(df, 1, strata, reference, target, period)
+    df[f"mc_flag_{target}"] = df[f"{target}_man"].notna()
 
-    df["bir_flag"] = flag_rolling_impute(df, 1, strata, reference, target, period)
+    df[f"fir_flag_{target}"] = flag_rolling_impute(
+        df, 1, strata, reference, target, period
+    )
+
+    df[f"bir_flag_{target}"] = flag_rolling_impute(
+        df, -1, strata, reference, target, period
+    )
+
+    df[f"fimc_flag_{target}"] = flag_rolling_impute(
+        df, 1, strata, reference, f"{target}_man", period
+    )
 
     construction_conditions = df[target].isna() & df[auxiliary].notna()
-    df["c_flag"] = np.where(construction_conditions, True, False)
+    df[f"c_flag_{target}"] = np.where(construction_conditions, True, False)
 
-    df["fic_flag"] = flag_rolling_impute(df, 1, strata, reference, auxiliary, period)
+    df[f"fic_flag_{target}"] = flag_rolling_impute(
+        df, 1, strata, reference, auxiliary, period
+    )
 
-    df.drop
+    return df
+
+
+def generate_imputation_marker(df: pd.DataFrame, target) -> pd.DataFrame:
+    """
+    Function to add column containing the a string indicating the method of
+    imputation to use following the hierarchy in specifications
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing logical columns produced by `create_imputation_flags`
+        (r_flag, fir_flag, bir_flag, fic_flag and c_flag)
+    target : str
+        the name of the column used for imputation
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with additional column containing imputation marker
+        i.e. the type of imputation method that should be used to fill
+        missing returns.
+    """
+
+    flags = ["r", "mc", "fir", "bir", "fimc", "c", "fic"]
+    select_cols = [f"{i}_flag_{target}" for i in flags]
+    first_condition_met = [np.where(i)[0][0] for i in df[select_cols].values]
+    df[f"imputation_flags_{target}"] = [flags[i] for i in first_condition_met]
 
     return df
 
@@ -95,53 +134,17 @@ def flag_rolling_impute(df, time_difference, strata, reference, target, period):
     pd.Series
     """
 
+    if time_difference < 0:
+        fillmethod = "bfill"
+    elif time_difference > 0:
+        fillmethod = "ffill"
+
     return (
-        df.groupby([strata, reference])
-        .shift(time_difference)[target]
+        df.groupby([strata, reference])[target]
+        .fillna(method=fillmethod)
         .notnull()
         .mul(
             df[period] - pd.DateOffset(months=time_difference)
             == df.shift(time_difference)[period]
         )
     )
-
-
-def generate_imputation_marker(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Function to add column containing the a string indicating the method of
-    imputation to use following the hierarchy in specifications
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame containing logical columns produced by `create_imputation_flags`
-        (r_flag, fir_flag, bir_flag, fic_flag and c_flag)
-
-
-    Returns
-    -------
-    pd.DataFrame
-        Dataframe with additional column containing imputation marker
-        i.e. the type of imputation method that should be used to fill
-        missing returns.
-    """
-
-    imputation_markers_and_conditions = {
-        "r": df["r_flag"],
-        "fir": ~df["r_flag"] & df["fir_flag"],
-        "bir": ~df["r_flag"] & ~df["fir_flag"] & df["bir_flag"],
-        "fic": ~df["r_flag"] & ~df["fir_flag"] & ~df["bir_flag"] & df["fic_flag"],
-        "c": ~df["r_flag"]
-        & ~df["fir_flag"]
-        & ~df["bir_flag"]
-        & ~df["fic_flag"]
-        & df["c_flag"],
-    }
-
-    df["imputation_marker"] = np.select(
-        imputation_markers_and_conditions.values(),
-        imputation_markers_and_conditions.keys(),
-        default="error",
-    )
-
-    return df
