04de3d5ad9994e265319c9faef69d62469b10508
hubbal <Luke.Hubbard@ons.gov.uk>
2024-07-12 15:03:30 +0100

    Writing aggregate_aweight function

---
+++ mbs_results/aggregate_aweight.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def aggregate_aweight(
+    dataframe: pd.DataFrame,
+    reference: str,
+    imp_class: str,
+    period: str,
+    a_weight: str,
+    current_period: int,
+) -> pd.DataFrame:
+  """
+  Returning imp_class and a_weight for current period based on reference dataframe.
+
+  Parameters
+  ----------
+  dataframe : pd.DataFrame
+      Reference dataframe with imp_class and a_weights
+  reference : str
+      name of column in dataframe containing reference variable
+  imp_class : str
+      name of column in dataframe containing imp_class variable
+  period : str
+      name of column in dataframe containing period variable
+  a_weight : str
+      name of column in dataframe containing a_weight variable
+  current_period : int
+      Current period to take a_weights for, in the format yyyymm
+
+  Returns
+  -------
+  pd.DataFrame
+      dataframe with aggregated imp_class and a_weights values for current period.
+
+  """
+  current_df = dataframe[dataframe['period'] == current_period]
+  
+  aggregate_df = current_df[['imp_class', 'a_weight']].drop_duplicates()
+  
+  return aggregate_df.reset_index(drop=True)
--- tests/test_aggregate_aweights.py
+++ tests/test_aggregate_aweights.py
@@ -4,29 +4,37 @@ import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
 
-#import new function here
+from mbs_results.aggregate_aweight import aggregate_aweight
+
 
 @pytest.fixture(scope="class")
 def filepath():
     return Path("tests/data/estimation")
-  
+
 
 @pytest.fixture(scope="class")
 def aweights_test_input(filepath):
-    return pd.read_csv(filepath / "class_aweights_input.csv", index_col=False)
-  
-  
+    return pd.read_csv(filepath / "aggregate_aweights_input.csv", index_col=False)
+
+
 @pytest.fixture(scope="class")
 def aweights_test_output(filepath):
-    return pd.read_csv(filepath / "class_aweights_output.csv", index_col=False)
-  
+    return pd.read_csv(filepath / "aggregate_aweights_output.csv", index_col=False)
+
 
 class TestAggregateAWeights:
-    def test_construction_matches_flag(self, construction_test_data):
+    def test_aggregate_aweights(self, aweights_test_input, aweights_test_output):
         expected_output = aweights_test_output
 
         input_data = aweights_test_input
-        
-        actual_output = #Function with arguments to go here
 
-        assert_frame_equal(actual_output, expected_output)  
+        actual_output = aggregate_aweight(
+            input_data,
+            "reference",
+            "imp_class",
+            "period",
+            "a_weight",
+            202402,
+        )
+
+        assert_frame_equal(actual_output, expected_output)
