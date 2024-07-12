f921b640b7bd0d7d69a53037be6e06e19cb98487
matthk <Katey.Matthews@ons.gov.uk>
2024-07-12 11:30:18 +0100

    adding data for aweight aggregation

---
+++ tests/Untitled.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+
+#import new function here
+
+@pytest.fixture(scope="class")
+def aweights_test_data():
+    return load_and_format(Path("tests") / "construction_matches.csv")
+
+class TestConstructionMatches:
+    def test_construction_matches_flag(self, construction_test_data):
+        expected_output = construction_test_data[
+            [
+                "target",
+                "period",
+                "auxiliary",
+                "flag_construction_matches",
+            ]
+        ]
+
+        input_data = expected_output.drop(columns=["flag_construction_matches"])
+        actual_output = flag_construction_matches(
+            input_data, "target", "period", "auxiliary"
+        )
+
+        assert_frame_equal(actual_output, expected_output)  
