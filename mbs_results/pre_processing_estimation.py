70b7c0d0baaf768f75c28b056947aea513d032e2
daviel9 <Luke.Davies@ons.gov.uk>
2024-07-17 16:46:59 +0100

    Add function to get estimation data

--- mbs_results/pre_processing_estimation.py
+++ mbs_results/pre_processing_estimation.py
@@ -1,3 +1,49 @@
+from glob import glob
+
+import pandas as pd
+from utils import read_colon_separated_file
+
+
+def get_estimation_data(
+    population_path,
+    population_column_names,
+    sample_path,
+    sample_column_names,
+    calibration_group_map,
+    period,
+    reference,
+    cell_number,
+    **config
+):
+
+    population_files = glob("universe.*", root_dir=population_path)
+    sample_files = glob("finalsel.*", root_dir=sample_path)
+
+    population_dfs = []
+    for file in population_files:
+
+        population_df = read_colon_separated_file(file, population_column_names)
+
+        population_dfs.append(population_df)
+
+    population = pd.concat(population_dfs, ignore_index=True)
+
+    sample_dfs = []
+    for file in sample_files:
+
+        sample_df = read_colon_separated_file(file, sample_column_names)
+
+        sample_dfs.append(sample_df)
+
+    sample = pd.concat(sample_dfs, ignore_index=True)
+
+    estimation_data = derive_estimation_variables(
+        population, sample, calibration_group_map, period, reference, cell_number
+    )
+
+    return estimation_data
+
+
 def derive_estimation_variables(
     population_frame,
     sample,
