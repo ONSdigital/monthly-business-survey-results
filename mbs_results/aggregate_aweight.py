e5debcee34f945ab0d10131a1146c374a20cba67
matthk <Katey.Matthews@ons.gov.uk>
2024-07-17 12:31:14 +0100

    Adding merge domain function

--- mbs_results/aggregate_aweight.py
+++ mbs_results/aggregate_aweight.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def aggregate_aweight(
     dataframe: pd.DataFrame,
     reference: str,
@@ -8,32 +9,32 @@ def aggregate_aweight(
     a_weight: str,
     current_period: int,
 ) -> pd.DataFrame:
-  """
-  Returning imp_class and a_weight for current period based on reference dataframe.
+    """
+    Returning imp_class and a_weight for current period based on reference dataframe.
 
-  Parameters
-  ----------
-  dataframe : pd.DataFrame
-      Reference dataframe with imp_class and a_weights
-  reference : str
-      name of column in dataframe containing reference variable
-  imp_class : str
-      name of column in dataframe containing imp_class variable
-  period : str
-      name of column in dataframe containing period variable
-  a_weight : str
-      name of column in dataframe containing a_weight variable
-  current_period : int
-      Current period to take a_weights for, in the format yyyymm
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        Reference dataframe with imp_class and a_weights
+    reference : str
+        name of column in dataframe containing reference variable
+    imp_class : str
+        name of column in dataframe containing imp_class variable
+    period : str
+        name of column in dataframe containing period variable
+    a_weight : str
+        name of column in dataframe containing a_weight variable
+    current_period : int
+        Current period to take a_weights for, in the format yyyymm
 
-  Returns
-  -------
-  pd.DataFrame
-      dataframe with aggregated imp_class and a_weights values for current period.
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with aggregated imp_class and a_weights values for current period.
 
-  """
-  current_df = dataframe[dataframe['period'] == current_period]
-  
-  aggregate_df = current_df[['imp_class', 'a_weight']].drop_duplicates()
-  
-  return aggregate_df.reset_index(drop=True)
+    """
+    current_df = dataframe[dataframe["period"] == current_period]
+
+    aggregate_df = current_df[["imp_class", "a_weight"]].drop_duplicates()
+
+    return aggregate_df.reset_index(drop=True)
---
+++ mbs_results/merge_domain.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def merge_domain(
+    input_df: pd.DataFrame,
+    domain_mapping: pd.DataFrame,
+    SIC_input: str,
+    SIC_mapping: str,
+) -> pd.DataFrame:
+    """
+    Returning a merged dataframe including SIC and domain from the domain mapping dataframe.
+
+    Parameters
+    ----------
+    input_df : pd.DataFrame
+        Dataframe containing reference, imp_class, period and SIC columns
+    domain_mapping : pd.DataFrame
+        Dataframe containing SIC and domain columns
+    SIC_input : str
+        name of column in input_df dataframe containing SIC variable
+    SIC_mapping : str
+        name of column in domain_mapping dataframe containing SIC variable
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with SIC and domain columns merged.
+
+    """
+
+    merged_df = input_df.merge(
+        domain_mapping, how="left", left_on=SIC_input, right_on=SIC_mapping
+    )
+
+    return merged_df.drop(columns=SIC_mapping)
