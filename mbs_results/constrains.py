a52994b4e6c14137c44d3403a20745df179bf825
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-07-29 18:01:25 +0100

    Feat sum dfs funciton
    * Add function which sums dfs on common indices
    * Add test scenario
    * Add test data

--- mbs_results/constrains.py
+++ mbs_results/constrains.py
@@ -1,25 +1,21 @@
-import pandas as pd
 import operator
-import numpy as np
 from typing import List
+
+import pandas as pd
 
 
 def replace_values_index_based(
-        df: pd.DataFrame,
-        target: str,
-        a: int,
-        compare: str,
-        b: int
+    df: pd.DataFrame, target: str, a: int, compare: str, b: int
 ) -> None:
     """
     Perform comparisons between a subset of df with a and subset of df with b and
     replace target from a with target from b when comparison is met. Both a
-    and b must exist in the first level index, the comparison is based on the 
+    and b must exist in the first level index, the comparison is based on the
     remaining indices.
-    
-    Note that this function does not return anything, it modifies the input 
+
+    Note that this function does not return anything, it modifies the input
     dataframe.
-    
+
     Parameters
     ----------
     df : pd.DataFrame
@@ -35,28 +31,60 @@ def replace_values_index_based(
     """
     # For improved perfomance
     df.sort_index(inplace=True)
-    
-    ops = {'>': operator.gt,
-       '<': operator.lt,
-       '>=': operator.ge,
-       '<=': operator.le,
-       '==': operator.eq}
-    
+
+    ops = {
+        ">": operator.gt,
+        "<": operator.lt,
+        ">=": operator.ge,
+        "<=": operator.le,
+        "==": operator.eq,
+    }
+
     series_from_a = df.loc[a][target]
-    
+
     series_from_b = df.loc[b][target]
-    
+
     common_index = series_from_a.index.intersection(series_from_b.index)
-    
-    # Has format (period,reference) 
-    index_to_replace = series_from_a[ops[compare](series_from_a[common_index] ,series_from_b[common_index])].index
-    
-    if len(index_to_replace)>0:
+
+    # Has format (period,reference)
+    index_to_replace = series_from_a[
+        ops[compare](series_from_a[common_index], series_from_b[common_index])
+    ].index
+
+    if len(index_to_replace) > 0:
         for date_ref_idx in index_to_replace.values:
-           
-            # Has format (question,no,period,reference) 
+
+            # Has format (question,no,period,reference)
             index_to_replace = (a,) + date_ref_idx
             index_to_replace_with = (b,) + date_ref_idx
             # Filter target based on the indices
-            df.loc[index_to_replace,target] = df.loc[index_to_replace_with,target]
-            df.loc[index_to_replace,"constrain_marker"]=f"{a} {compare} {b}"
+            df.loc[index_to_replace, target] = df.loc[index_to_replace_with, target]
+            df.loc[index_to_replace, "constrain_marker"] = f"{a} {compare} {b}"
+
+
+def sum_sub_df(df: pd.DataFrame, derive_from: List[int]) -> pd.DataFrame:
+    """
+    Returns the sums of a dataframe in which the first level index is in
+    derive_from. The sum is based on common indices. Columns must contain float
+    or int.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe to sum, first level index must contain values from derive_from
+
+    derive_from : List[int]
+        Values to take a subset of df.
+
+    Returns
+    -------
+    sums : pd.DataFrame
+        A dataframe with sums, constain marker, and columns from index which the
+        sum was based on.
+    """
+
+    sums = sum(
+        [df.loc[question_no] for question_no in derive_from if question_no in df.index]
+    )
+
+    return sums.assign(constrain_marker=f"sum{derive_from}").reset_index()
--- tests/test_constrains.py
+++ tests/test_constrains.py
@@ -1,25 +1,48 @@
-from pathlib import Path
 import pandas as pd
-import pytest
 from pandas.testing import assert_frame_equal
 
-from mbs_results.constrains import replace_values_index_based
+from mbs_results.constrains import replace_values_index_based, sum_sub_df
 
 
 class TestConstrains:
     def test_replace_values_index_base(self):
 
-        df = pd.read_csv("tests/test_replace_values_index_based.csv",index_col=False)
+        df = pd.read_csv("tests/test_replace_values_index_based.csv", index_col=False)
 
-        df =  df.set_index(["question_no","period","reference"])
+        df = df.set_index(["question_no", "period", "reference"])
 
-        df_in = df[['target']].copy()
+        df_in = df[["target"]].copy()
 
-        df_expected = df[['expected', 'constrain_marker']].rename(columns = {"expected":"target"})
+        df_expected = df[["expected", "constrain_marker"]].rename(
+            columns={"expected": "target"}
+        )
         df_expected.sort_index(inplace=True)
 
-        replace_values_index_based(df_in,"target",49,'>',40)
-        replace_values_index_based(df_in,"target",90,'>=',40)
+        replace_values_index_based(df_in, "target", 49, ">", 40)
+        replace_values_index_based(df_in, "target", 90, ">=", 40)
 
         assert_frame_equal(df_in, df_expected)
 
+    def test_sum_sub_df_46_47(self):
+
+        df = pd.read_csv("tests/test_sum_sub_df.csv", index_col=False)
+
+        df_in = (
+            df.loc[df["question_no"] != 40]
+            .drop(columns=["constrain_marker"])
+            .set_index(["question_no", "period", "reference"])
+        )
+
+        expected_output = (
+            df.loc[df["question_no"] == 40]
+            .drop(columns=["question_no"])
+            .reset_index(drop=True)
+        )
+
+        actual_ouput = (
+            sum_sub_df(df_in, [46, 47])
+            .sort_values(by="reference")
+            .reset_index(drop=True)
+        )
+
+        assert_frame_equal(actual_ouput, expected_output)
