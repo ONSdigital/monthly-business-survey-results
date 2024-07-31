6fad8d0ae81d1d65a241b76dc1006823ed64a5f4
hubbal <Luke.Hubbard@ons.gov.uk>
2024-07-31 11:49:13 +0100

    Changing merge_counts function to join on different column names

--- mbs_results/pivot_imputation_value.py
+++ mbs_results/pivot_imputation_value.py
@@ -2,7 +2,15 @@ import numpy as np
 import pandas as pd
 
 
-def merge_counts(input_df: pd.DataFrame, count_df: pd.DataFrame) -> pd.DataFrame:
+def merge_counts(
+    input_df: pd.DataFrame,
+    count_df: pd.DataFrame,
+    input_cell: str,
+    count_cell: str,
+    input_date: str,
+    count_date: str,
+    identifier: str,
+) -> pd.DataFrame:
     """
     Returns input data with f_count and b_count merged on.
 
@@ -12,30 +20,37 @@ def merge_counts(input_df: pd.DataFrame, count_df: pd.DataFrame) -> pd.DataFrame
         Reference dataframe with identifier, date, sic, cell, forward, backward, construction, question,imputed_value
     count_df : pd.DataFrame
         DataFrame with group, period, flag_1 and flag_2.
-    input_cell :
+    input_cell : str
         name of column in input_df dataframe containing cell variable
-    count_cell :
+    count_cell : str
         name of column in count_df dataframe containing cell variable
-    input_date :
+    input_date : str
         name of column in input_df dataframe containing date variable
-    count_date :
+    count_date : str
         name of column in count_df dataframe containing date variable
-    Returns
+    identifier : str
+        name of column in input_df containing identifier variable
+
     Returns
     -------
-    Dataframe resulting from the left-join of df_2 and df_1 (after renaming columns), on 'cell' and 'date'.
+    Dataframe resulting from the left-join of input_df and count_df (after renaming columns),
+    on the cell and date columns.
     """
     count_df = count_df.rename(
         columns={
-            "group": "cell",
-            "period": "date",
             "flag_1": "f_count",
             "flag_2": "b_count",
         }
     )
-    df_merge = pd.merge(input_df, count_df, how="left", on=["cell", "date"])
-    df_merge["identifier"] = df_merge["identifier"].astype(int)
-    return df_merge
+    df_merge = pd.merge(
+        input_df,
+        count_df,
+        how="left",
+        left_on=[input_cell, input_date],
+        right_on=[count_cell, count_date],
+    ).astype({identifier: "int"})
+
+    return df_merge.drop(columns=[count_cell, count_date])
 
 
 def pivot_imputation_value(
@@ -66,7 +81,7 @@ def pivot_imputation_value(
     date : str
         name of column in dataframe containing period variable
     sic : str
-        name of column in dataframe containing domain variable
+        name of column in dataframe containing question code variable
     cell : str
         name of column in dataframe containing question code variable
     forward : str
--- tests/test_pivot_imputation_value.py
+++ tests/test_pivot_imputation_value.py
@@ -14,21 +14,13 @@ def filepath():
 
 
 @pytest.fixture(scope="class")
-def merge_counts_input(filepath):
-    input_df = pd.read_csv(
-        filepath / "data" / "pivot_imputation_value_input.csv", index_col=False
-    )
-    count_df = pd.read_csv(
-        filepath / "data" / "pivot_imputation_value_counts_input.csv", index_col=False
-    )
-    return (input_df, count_df)
+def count_data_input(filepath):
+    return pd.read_csv(filepath / "data" / "count_data_input.csv", index_col=False)
 
 
 @pytest.fixture(scope="class")
 def merge_counts_output(filepath):
-    return pd.read_csv(
-        filepath / "data" / "pivot_imputation_value_input_2.csv", index_col=False
-    )
+    return pd.read_csv(filepath / "data" / "merge_counts_output.csv", index_col=False)
 
 
 @pytest.fixture(scope="class")
@@ -39,10 +31,15 @@ def pivot_imputation_value_output(filepath):
 
 
 class TestMergeCounts:
-    def test_merge_counts(self, merge_counts_input, merge_counts_output):
-        input_df, count_df = merge_counts_input
-        actual_output = merge_counts(input_df, count_df)
+    def test_merge_counts(self, count_data_input, merge_counts_output):
+
+        input_df = merge_counts_output.drop(columns=["f_count", "b_count"])
+
+        actual_output = merge_counts(
+            input_df, count_data_input, "cell", "group", "date", "period", "identifier"
+        )
         expected_output = merge_counts_output
+
         assert_frame_equal(actual_output, expected_output)
 
 
@@ -50,7 +47,9 @@ class TestPivotImputationValue:
     def test_pivot_imputation_value(
         self, pivot_imputation_value_output, merge_counts_output
     ):
-        expected_output = pivot_imputation_value_output.reset_index(drop=True)
+
+        expected_output = pivot_imputation_value_output
+
         expected_output["link_type"] = pd.Categorical(
             expected_output["link_type"], categories=["F", "B", "C"], ordered=True
         )
