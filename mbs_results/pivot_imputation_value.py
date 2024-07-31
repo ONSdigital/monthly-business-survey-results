9c1e76a27ea62e75b666b65a097d9b41304063ab
matthk <Katey.Matthews@ons.gov.uk>
2024-07-31 08:51:52 +0100

    Adding functions in pivot_imputation_value for imputation_links output

--- mbs_results/pivot_imputation_value.py
+++ mbs_results/pivot_imputation_value.py
@@ -1,4 +1,41 @@
+import numpy as np
 import pandas as pd
+
+
+def merge_counts(input_df: pd.DataFrame, count_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns input data with f_count and b_count merged on.
+
+    Parameters
+    ----------
+    input_df : pd.DataFrame
+        Reference dataframe with identifier, date, sic, cell, forward, backward, construction, question,imputed_value
+    count_df : pd.DataFrame
+        DataFrame with group, period, flag_1 and flag_2.
+    input_cell :
+        name of column in input_df dataframe containing cell variable
+    count_cell :
+        name of column in count_df dataframe containing cell variable
+    input_date :
+        name of column in input_df dataframe containing date variable
+    count_date :
+        name of column in count_df dataframe containing date variable
+    Returns
+    Returns
+    -------
+    Dataframe resulting from the left-join of df_2 and df_1 (after renaming columns), on 'cell' and 'date'.
+    """
+    count_df = count_df.rename(
+        columns={
+            "group": "cell",
+            "period": "date",
+            "flag_1": "f_count",
+            "flag_2": "b_count",
+        }
+    )
+    df_merge = pd.merge(input_df, count_df, how="left", on=["cell", "date"])
+    df_merge["identifier"] = df_merge["identifier"].astype(int)
+    return df_merge
 
 
 def pivot_imputation_value(
@@ -11,8 +48,11 @@ def pivot_imputation_value(
     backward: str,
     construction: str,
     question: str,
+    imputed_value: str,
+    f_count: str,
+    b_count: str,
 ) -> pd.DataFrame:
-  
+
     """
     Returning dataframe containing imputation_value, filtered by date, pivoted by imputation type
     and grouped by sic, cell, question and imputation type.
@@ -37,27 +77,77 @@ def pivot_imputation_value(
         name of column in dataframe containing a_weight variable
     question : str
         name of column in dataframe containing o_weight variable
-    
+    imputed_value: str
+        name of column in dataframe containing imputed_value variable
+    f_count: str,
+        name of column in dataframe containing f_count variable
+    b_count: str,
+        name of column in dataframe containing b_count variable
+
     Returns
     -------
     dataframe filtered by date, containing imputation_value, pivoted by imputation type
     and grouped by sic, cell, question and imputation type.
 
     """
-    
-    df = df[df.date == 202001]
 
-    df = df.drop_duplicates(subset=['date', 'sic', 'cell', 'question'])
+    df = df[df[date] == 202001]
 
-    df = df.melt(id_vars=['date', 'sic', 'cell', 'question'],
-                    value_vars=['forward', 'backward', 'construction'],
-                    var_name='link_type',
-                    value_name='imputation_link')
+    df1 = df.melt(
+        id_vars=[date, sic, cell, question, imputed_value],
+        value_vars=[forward, backward, construction],
+        var_name="link_type",
+        value_name="imputation_link",
+    )
 
-    link_type_map = {'forward': 'F', 'backward': 'B', 'construction': 'C'}
-    df['link_type'] = df['link_type'].map(link_type_map)
+    link_type_map = {forward: "F", backward: "B", construction: "C"}
+    df1["link_type"] = df1["link_type"].map(link_type_map)
 
-    df['link_type'] = pd.Categorical(df['link_type'], categories=['F','B','C'], ordered=True)
-    df.sort_values(['date', 'sic', 'cell', 'question', 'link_type'], inplace=True)
+    df2 = df.melt(
+        id_vars=[date, sic, cell, question],
+        value_vars=[f_count, b_count],
+        var_name="link_type_count",
+        value_name="count",
+    )
 
-    return df.reset_index(drop=True)
+    link_type_map_count = {f_count: "F", b_count: "B"}
+    df2["link_type_count"] = df2["link_type_count"].map(link_type_map_count)
+
+    merged_df = pd.merge(
+        df1,
+        df2,
+        how="outer",
+        left_on=[date, sic, cell, question, "link_type"],
+        right_on=[date, sic, cell, question, "link_type_count"],
+    )
+
+    merged_df.drop_duplicates(inplace=True)
+    merged_df.drop(["link_type_count"], axis=1, inplace=True)
+
+    merged_df = merged_df.groupby(
+        [date, sic, cell, question, "link_type"], as_index=False
+    ).agg({imputed_value: "sum", "count": "first", "imputation_link": "first"})
+
+    sorting_order = {"F": 1, "B": 2, "C": 3}
+    merged_df["sort_column"] = merged_df["link_type"].map(sorting_order)
+
+    merged_df = merged_df.sort_values([date, sic, cell, question, "sort_column"])
+
+    merged_df.drop("sort_column", axis=1, inplace=True)
+
+    merged_df.reset_index(drop=True, inplace=True)
+
+    merged_df = merged_df[
+        [
+            date,
+            sic,
+            cell,
+            question,
+            "imputation_link",
+            "link_type",
+            "count",
+            imputed_value,
+        ]
+    ]
+
+    return merged_df
--- tests/test_pivot_imputation_value.py
+++ tests/test_pivot_imputation_value.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import pytest
 from pandas.testing import assert_frame_equal
 
-from mbs_results.pivot_imputation_value import pivot_imputation_value
+from mbs_results.pivot_imputation_value import merge_counts, pivot_imputation_value
 
 
 @pytest.fixture(scope="class")
@@ -13,40 +14,48 @@ def filepath():
 
 
 @pytest.fixture(scope="class")
-def pivot_imputation_value_input(filepath):
-    return pd.read_csv(
-        filepath / "data"/"pivot_imputation_value_input.csv", index_col=False
+def merge_counts_input(filepath):
+    input_df = pd.read_csv(
+        filepath / "data" / "pivot_imputation_value_input.csv", index_col=False
     )
-  
+    count_df = pd.read_csv(
+        filepath / "data" / "pivot_imputation_value_counts_input.csv", index_col=False
+    )
+    return (input_df, count_df)
+
+
+@pytest.fixture(scope="class")
+def merge_counts_output(filepath):
+    return pd.read_csv(
+        filepath / "data" / "pivot_imputation_value_input_2.csv", index_col=False
+    )
+
+
 @pytest.fixture(scope="class")
 def pivot_imputation_value_output(filepath):
     return pd.read_csv(
-        filepath / "data"/"pivot_imputation_value_output.csv", index_col=False
+        filepath / "data" / "pivot_imputation_value_output.csv", index_col=False
     )
+
+
+class TestMergeCounts:
+    def test_merge_counts(self, merge_counts_input, merge_counts_output):
+        input_df, count_df = merge_counts_input
+        actual_output = merge_counts(input_df, count_df)
+        expected_output = merge_counts_output
+        assert_frame_equal(actual_output, expected_output)
 
 
 class TestPivotImputationValue:
     def test_pivot_imputation_value(
-        self,
-        pivot_imputation_value_input,
-        pivot_imputation_value_output
+        self, pivot_imputation_value_output, merge_counts_output
     ):
-        expected_output = pivot_imputation_value_output[
-           [
-                "date",
-                "sic",
-                "cell",
-                "question",
-                "link_type",
-                "imputation_link",
-            ]
-        ].reset_index(drop=True)
-        
-        expected_output["link_type"] = pd.Categorical(expected_output["link_type"], categories=["F", "B", "C"], ordered=True)
-
-        input_data = pivot_imputation_value_input.drop(
-            columns=["identifier"]
+        expected_output = pivot_imputation_value_output.reset_index(drop=True)
+        expected_output["link_type"] = pd.Categorical(
+            expected_output["link_type"], categories=["F", "B", "C"], ordered=True
         )
+
+        input_data = merge_counts_output.drop(columns=["identifier"])
 
         actual_output = pivot_imputation_value(
             input_data,
@@ -58,6 +67,13 @@ class TestPivotImputationValue:
             "backward",
             "construction",
             "question",
+            "imputed_value",
+            "f_count",
+            "b_count",
+        )
+
+        actual_output["link_type"] = pd.Categorical(
+            actual_output["link_type"], categories=["F", "B", "C"], ordered=True
         )
 
         assert_frame_equal(actual_output, expected_output)
