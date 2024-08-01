f85309377065cd1ede189a73f22715dac4acb7b9
Anton Zogkolli <anton.zogkolli@ons.gov.uk>
2024-08-01 16:18:51 +0100

    Scripts for testing

---
+++ get_test_outputs.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 12:24:43 2024
+
+@author: zogkoa
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+import json
+
+from testing_helpers import get_pre_impute_data, proccess_for_pre_impute, check_na_duplicates,map_form_type
+
+# pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git
+
+from mbs_results.data_cleaning import create_imputation_class
+from mbs_results.ratio_of_means import ratio_of_means
+from mbs_results.constrains import constrain
+from mbs_results.apply_estimation import apply_estimation
+
+from mbs_results.flag_for_winsorisation import winsorisation_flag #1
+from mbs_results.calculate_predicted_unit_value import calculate_predicted_unit_value #2
+from mbs_results.calculate_ratio_estimation import calculate_ratio_estimation #3
+from mbs_results.calculate_winsorised_weight import calculate_winsorised_weight #4
+
+
+def wrap_winsorised(df,l_values_path):
+    """Temporary wrap"""
+
+    l_values = pd.read_csv(l_values_path)
+    
+    
+    df = pd.merge(df,l_values,how="left",left_on=["period","question_no","frosic2007"],right_on=["period","question_no","classification"])
+    
+    df1 = winsorisation_flag(df,"design_weight","calibration_factor")
+    
+    df2 = calculate_predicted_unit_value(df1,"frotover_x","sampled","design_weight","adjusted_value",'nw_ag_flag')
+    
+    df3 = calculate_ratio_estimation(df2,"frotover_x","sampled","design_weight","calibration_factor","adjusted_value","predicted_unit_value",
+                                     "l_value",'nw_ag_flag')
+    
+    df4 = calculate_winsorised_weight(df3,"cell_no_x","period","frotover_x","sampled","design_weight","calibration_factor","adjusted_value","predicted_unit_value",
+                                      "l_value","ratio_estimation_treshold","nw_ag_flag")
+
+
+    return df4
+
+def load_config():
+        with open('test_outputs_config.json') as f:
+            return json.load(f)
+    
+if __name__ == "__main__":
+    
+    FILE_VERSION = "v0.0.0"
+    
+    config = load_config()
+    config['calibration_group_map'] = pd.read_csv(config['calibration_group_map'])
+    
+    pre_impute_df = get_pre_impute_data(
+        config['df_path'],config['df_path'],config['sample_path'],config['sample_column_names'])
+    
+    pre_impute_df = proccess_for_pre_impute(pre_impute_df)
+    
+    check_na_duplicates(pre_impute_df) #just basic check
+    
+    pre_impute_df = create_imputation_class(pre_impute_df,"cell_no","class")
+
+    post_impute = pre_impute_df.groupby("question_no").apply(
+        lambda df:ratio_of_means(
+            df = df,
+            reference="reference",
+            target="adjusted_value",
+            period="period",
+            strata = "class",
+            auxiliary="frotover"))
+
+    post_impute['period']  =  post_impute['period'].dt.strftime('%Y%m').astype("int")
+
+    post_impute = post_impute.reset_index(drop=True) #remove groupby leftovers
+
+    post_impute = map_form_type(post_impute)
+    
+
+    post_constrain = constrain(post_impute,"period","reference","adjusted_value","imputed_value","question_no","form_type_spp")
+    
+    # Imputation test data here
+    post_constrain.to_csv(config['out_path']+f"imputation_{FILE_VERSION}.csv",index=False)
+    
+    estimate_df = apply_estimation(**config)
+
+    # cell_no frotover exist in both dfs we load twice finalsel
+    # cell_no_x is original no changes, cell_no_y from estimation (some changes applied for NI and UK)
+    # frotover is the same on both
+    
+    post_estimate = pd.merge(post_constrain,estimate_df,how = "left",on = ["period","reference"])
+    
+    estimate_out = post_estimate[["period","cell_no_y","calibration_group","design_weight","calibration_factor"]]
+    
+    estimate_out.to_csv(config['out_path']+f"estimation_{FILE_VERSION}.csv",index=False)
+    
+    post_win = wrap_winsorised(post_estimate,config['l_values_path'])
+    
+    post_win.to_csv(config['out_path']+f"winsorisation_{FILE_VERSION}.csv",index=False)
+
+    
---
+++ testing_helpers.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 11:56:43 2024
+
+@author: zogkoa
+"""
+
+import fnmatch
+import re
+from io import BytesIO
+from os import listdir
+from os.path import isfile, join
+from typing import List
+import pandas as pd
+import numpy as np
+import glob
+	
+# pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git
+from mbs_results.utils import read_colon_separated_file, convert_column_to_datetime
+
+
+def get_patern_df(filepath, pattern):
+    """Loads as pd dataframe all csv files with pattern"""
+    
+    filenames = [
+        filename for filename in listdir(filepath) if isfile(join(filepath, filename))
+    ]
+    filenames = fnmatch.filter(filenames, pattern)
+    df_list = [pd.read_csv(filepath + filename) for filename in filenames]
+    df = pd.concat(df_list, ignore_index=True)
+
+    return df
+
+
+
+def get_pre_impute_data(cp_path,qv_path,finalsel_path,finalsel_columns):
+    """Reads qv and left joins cp and finalsel data to qv"""
+    
+    qv_df = get_patern_df(qv_path,"qv*.csv")
+    cp_df = get_patern_df(cp_path,"cp*.csv")
+
+    df_finalsel = pd.concat(
+        [read_colon_separated_file(f,finalsel_columns) for f in  glob.glob(finalsel_path)]
+        ,ignore_index=True) 
+    
+    qv_df['period'] = qv_df[["period","Period"]].agg(sum,axis=1)
+    qv_df = qv_df.drop(columns=["Period"])
+    
+    qv_and_cp = pd.merge(qv_df,cp_df,how = "left",on = ["period","reference"])
+    pre_proccessed_df = pd.merge(qv_and_cp,df_finalsel,how = "left",on = ["period","reference"])
+    
+    return pre_proccessed_df
+
+
+def proccess_for_pre_impute(df):
+    """Makes nan values selects relevant columns and questions no"""
+    
+    df['period'] = convert_column_to_datetime(df['period'] )
+    df.loc[(df["response_type"]==1) & (df['period']!=df['period'].min()),"adjusted_value"] = np.nan
+    
+    df = df[['period', 'reference', 'question_no', 'adjusted_value',"cell_no","frotover","form_type"]]
+
+    questions = [40, 42, 43, 46, 47, 49, 90, 110]
+
+
+    df = df.loc[df['question_no'].isin(questions)]
+    
+    return df
+
+    
+def check_na_duplicates(df):
+    """Does basic checks"""
+    
+    print("nas: ",df.isna().sum()) #count na
+    mask = df.groupby(['period','reference','question_no'])['reference'].transform('size')>1
+    print("Sum of duplicates: ",len(df[mask])) # check for duplicates
+    
+    
+def map_form_type(df):
+    
+    form_type_map = {
+    "T117G":13,
+    "T167G":13,
+    "T123G":14,
+    "T173G":14,
+    "T817G":15,
+    "T867G":15,
+    "T823G":16,
+    "T873G":16
+    }
+
+ 
+    df["form_type"] = df["form_type"].str.strip()
+
+    df = df.assign(form_type_spp= df["form_type"].map(form_type_map))
+    
+    return df
