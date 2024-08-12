5b23471e6253b8d76984550a724e84a8ec903586
Jordan Day <jordan.day@ons.gov.uk>
2024-08-12 16:31:11 +0100

    first draft of l weight functions, will move into different modules once developed

--- mbs_results/calculate_winsorised_weight.py
+++ mbs_results/calculate_winsorised_weight.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pandas as pd
+
+from mbs_results.data_cleaning import (
+    convert_column_to_datetime,
+    validate_manual_constructions,
+)
 
 
 def calculate_winsorised_weight(
@@ -72,3 +78,71 @@ def calculate_winsorised_weight(
     df["new_target_variable"] = df["new_target_variable"].mask(non_winsorised, np.nan)
 
     return df
+
+
+def load_l_values(df: pd.DataFrame, reference, period, l_values_path, **config):
+    """
+    function to load the l values data and merge onto main df
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        dataframe with combined responses and contributors columns
+        index is expected to be 'period' and 'reference'
+    reference: str
+        the name of the reference column
+    period: str
+        the name of the period column
+    l_values_path: str
+        the location of the l values data
+    **config: Dict
+        main pipeline configuration. Can be used to input the entire config dictionary
+
+    Returns
+    -------
+    pd.DataFrame
+        dataframe with correctly formatted column datatypes.
+    """
+    l_values = pd.read_csv(l_values_path)
+    l_values[period] = convert_column_to_datetime(l_values[period])
+    l_values[reference] = l_values[reference].astype("str")
+    l_values.set_index([reference, period], inplace=True)
+
+    validate_manual_constructions(df, l_values)
+
+    return df.merge(
+        l_values,
+        on=[reference, period],
+        how="outer",
+    )
+
+
+def validate_l_values(df, l_values):
+    """
+    Checks that manual construction identifiers match those in the main dataset
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        the main dataframe after preprocessing
+    manual_constructions: pd.DataFrame
+        the manual constructions input read in as a dataframe
+
+    Raises
+    ------
+    ValueError
+        ValueError if any combinations of period and reference appear in the manual
+        constructions input but not in the main dataframe
+    """
+
+    incorrect_ids = set(l_values.index) - set(df.index)
+
+    if len(incorrect_ids) > 1:
+        string_ids = " ".join(
+            [f"\nreference: {str(i[0])}, period: {str(i[1])}" for i in incorrect_ids]
+        )
+
+        raise ValueError(
+            f"""There are reference and period combinations in the manual constructions
+      with no match: {string_ids}"""
+        )
