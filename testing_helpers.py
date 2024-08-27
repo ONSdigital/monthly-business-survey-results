13138b27548a82c00da80f609b953c94cb4b959a
Anton Zogkolli <anton.zogkolli@ons.gov.uk>
2024-08-27 16:07:29 +0100

    Adjust scripts for 0.0.2 version
    * Remove temp wins wrapper
    * Moved qa_482 function to helpers
    * Adjust pre-process function for new revised data

--- testing_helpers.py
+++ testing_helpers.py
@@ -4,6 +4,9 @@ from os.path import isfile, join
 import pandas as pd
 import numpy as np
 import glob
+
+import json
+
 	
 # pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git
 from mbs_results.utils import read_colon_separated_file, convert_column_to_datetime
@@ -33,9 +36,6 @@ def get_pre_impute_data(cp_path,qv_path,finalsel_path,finalsel_columns):
         [read_colon_separated_file(f,finalsel_columns) for f in  glob.glob(finalsel_path)]
         ,ignore_index=True) 
     
-    qv_df['period'] = qv_df[["period","Period"]].agg(sum,axis=1)
-    qv_df = qv_df.drop(columns=["Period"])
-    
     qv_and_cp = pd.merge(qv_df,cp_df,how = "left",on = ["period","reference"])
     pre_proccessed_df = pd.merge(qv_and_cp,df_finalsel,how = "left",on = ["period","reference"])
     
@@ -43,28 +43,53 @@ def get_pre_impute_data(cp_path,qv_path,finalsel_path,finalsel_columns):
 
 
 def proccess_for_pre_impute(df):
-    """Makes nan values selects relevant columns and questions no"""
+    """For ASAP-492"""
     
     df['period'] = convert_column_to_datetime(df['period'] )
-    df.loc[(df["response_type"]==1) & (df['period']!=df['period'].min()),"adjusted_value"] = np.nan
-    
-    df = df[[
-        'period', 'reference', 'question_no', 'adjusted_value',"cell_no",
-        "frotover","form_type","response_type","error_mkr","froempees"]]
 
     questions = [40, 42, 43, 46, 47, 49, 90, 110]
-    
+        
     df = df.loc[df['question_no'].isin(questions)]
     
-    df["form_type"] = df["form_type"].str.strip()
-
-    df.drop(df.loc[df['form_type'].isin(["T117G","T167G","T123G","T173G"]) & (df['question_no']==40)].index, inplace=True)
+    # has 60 columns, selecting only the ones we need for tidyness
+    df = df[[
+        'period', 'reference', 'question_no', 'adjusted_value',"cell_no",
+        "frotover","form_type","response_type","type","error_mkr","froempees"]]
     
-    df.drop(df.loc[df['form_type'].isin(["T823G","T873G"]) & (df['question_no']==42)].index, inplace=True)
-
-    df.drop(df.loc[df['form_type'].isin(["T817G","T867G"]) & (df['question_no']==46)].index, inplace=True)
+    # Updating response_type to 1 for 15399057545 , this is a known error check ASAP-492 for details
+    df.loc[df['reference']==15399057545,"response_type"] = 1
+    
+    zero_to_null_rules = (
+         (df["adjusted_value"]==0) 
+         & ((df["response_type"]>4)  #response_type >4 leave 0 
+        | ((df["response_type"]==2) & (df["type"]==1)) # or response_type 2 and type 1 leave 0
+            )        
+        )
+    
+    convert_to_null_rules = (
+        ((df["response_type"]==1) & (df["type"]==5)) #response_type 1 type 5 remove values
+        )
+    
+    remove_values_rules = (
+        (df["type"]==2) # drop type 2    
+        )
+    
+    df.drop(df[remove_values_rules].index, inplace=True)
+    
+    df.loc[zero_to_null_rules,"adjusted_value"] = np.nan
+    df.loc[convert_to_null_rules,"adjusted_value"] = np.nan
 
     df.reset_index(drop=True,inplace=True)
+
+    #Some checks to check if rules were applied
+    print("The below must not have values for type 2 since we dropped them\n",
+          df.groupby(["response_type","type"])["adjusted_value"].count()
+          )
+    
+    print("Selecting all the nas and zeros of adjusted value\n",
+        "The below must not have 0 for response_type 2 type 1 and response_type >4\n",
+        "Also for response type 1 and type 5 must have only nans (mannual costructed)\n",
+        df.loc[df["adjusted_value"].isin([np.nan,0])].groupby(["response_type","type"])["adjusted_value"].unique())
     
     return df
 
@@ -72,7 +97,7 @@ def proccess_for_pre_impute(df):
 def check_na_duplicates(df):
     """Does basic checks"""
     
-    print("nas: ",df.isna().sum()) #count na
+    print("nas: \n",df.isna().sum()) #count na
     mask = df.groupby(['period','reference','question_no'])['reference'].transform('size')>1
     print("Sum of duplicates: ",len(df[mask])) # check for duplicates
     
@@ -96,3 +121,33 @@ def map_form_type(df):
     df = df.assign(form_type_spp= df["form_type"].map(form_type_map))
     
     return df
+
+def get_qa_output_482(post_win_df):
+    """For ASAP-482"""
+    
+    requested_columns = [
+        'reference',"period","frosic2007",'classification','cell_no','frotover',
+        'froempees', 'form_type', 'response_type', 'question_no','adjusted_value',
+        'error_mkr', 'design_weight','calibration_factor', 'outlier_weight',
+        'total weight (A*G*O)','weighted adjusted value', 'imputation_flags_adjusted_value',
+        'imp_class','f_link_adjusted_value', 'f_match_adjusted_value_pair_count',
+        'default_link_f_match_adjusted_value','b_link_adjusted_value',
+        'b_match_adjusted_value_pair_count','default_link_b_match_adjusted_value',
+        'construction_link', 'flag_construction_matches_pair_count','default_link_flag_construction_matches',
+        "imputed_value","constrain_marker" # these not requested but usefull
+        ]
+
+    # not part of the pipeline the below
+    post_win_df['total weight (A*G*O)'] = post_win_df['design_weight']*post_win_df['calibration_factor']*post_win_df['outlier_weight']
+    
+    post_win_df['weighted adjusted value'] =post_win_df['imputed_value'] * post_win_df['total weight (A*G*O)']
+
+
+    return post_win_df[requested_columns]
+
+def load_config():
+        with open('test_outputs_config.json') as f:
+            return json.load(f)
+    
+
+
--- testing_main.py
+++ testing_main.py
@@ -1,62 +1,23 @@
 from importlib import metadata
 import pandas as pd
-import numpy as np
-import json
 
-from testing_helpers import get_pre_impute_data, proccess_for_pre_impute, check_na_duplicates,map_form_type
-from qa_output_482 import get_qa_output_482
+from testing_helpers import (
+    get_pre_impute_data, proccess_for_pre_impute,
+    check_na_duplicates,map_form_type,get_qa_output_482,
+    load_config
+    )
 
-# pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git
+# pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git@0.0.2
 
 from mbs_results.data_cleaning import create_imputation_class
 from mbs_results.ratio_of_means import ratio_of_means
 from mbs_results.constrains import constrain
 from mbs_results.apply_estimation import apply_estimation
+from mbs_results.winsorisation.py import winsorise
 
-from mbs_results.flag_for_winsorisation import winsorisation_flag #1
-from mbs_results.calculate_predicted_unit_value import calculate_predicted_unit_value #2
-from mbs_results.calculate_ratio_estimation import calculate_ratio_estimation #3
-from mbs_results.calculate_winsorised_weight import calculate_winsorised_weight #4
-
-
-def wrap_winsorised(df,l_values_path):
-    """Temporary wrap"""
-
-    l_values = pd.read_csv(l_values_path)
-    
-    l_values = l_values.drop_duplicates(['question_no','classification'])
-    
-    l_values = l_values.drop(columns=["period"])
-    # Imputed values are in a seperate column 
-    df["adjusted_value"] = df[["adjusted_value", "imputed_value"]].agg(
-          sum, axis=1
-      )
-    
-    df['frosic2007_3d'] = (pd.Series(np.floor(df['frosic2007']/100 )*100)).astype(int)
-
-    df1 = winsorisation_flag(df,"design_weight","calibration_factor")
-    
-    
-    df2 = calculate_predicted_unit_value(df1,"frosic2007_3d","period","frotover","sampled","design_weight","adjusted_value",'nw_ag_flag')
-    
-    df2 = pd.merge(df2,l_values,how="left",left_on=["question_no","frosic2007_3d"],right_on=["question_no","classification"])
-
-    
-    df3 = calculate_ratio_estimation(df2,"frotover","sampled","design_weight","calibration_factor","adjusted_value","predicted_unit_value",
-                                     "l_value",'nw_ag_flag')
-    
-    df4 = calculate_winsorised_weight(df3,"frosic2007_3d","period","frotover","sampled","design_weight","calibration_factor","adjusted_value","predicted_unit_value",
-                                      "l_value","ratio_estimation_treshold","nw_ag_flag")
-
-
-    return df4
-
-def load_config():
-        with open('test_outputs_config.json') as f:
-            return json.load(f)
     
 if __name__ == "__main__":
-    
+        
     FILE_VERSION = metadata.metadata("monthly-business-survey-results")["version"]
     
     config = load_config()
@@ -69,7 +30,7 @@ if __name__ == "__main__":
     
     check_na_duplicates(pre_impute_df) #just basic check
     
-    pre_impute_df = create_imputation_class(pre_impute_df,"cell_no","imp_class")
+    pre_impute_df = create_imputation_class(pre_impute_df,"cell_no","imputation_class")
 
     post_impute = pre_impute_df.groupby("question_no").apply(
         lambda df:ratio_of_means(
@@ -77,7 +38,7 @@ if __name__ == "__main__":
             reference="reference",
             target="adjusted_value",
             period="period",
-            strata = "imp_class",
+            strata = "imputation_class",
             auxiliary="frotover"))
 
     post_impute['period']  =  post_impute['period'].dt.strftime('%Y%m').astype("int")
@@ -88,9 +49,8 @@ if __name__ == "__main__":
 
     post_impute = map_form_type(post_impute)
     
-
     post_constrain = constrain(post_impute,"period","reference","adjusted_value","imputed_value","question_no","form_type_spp")
-    
+
     check_na_duplicates(post_constrain) #just basic check
 
     # Imputation test data here
@@ -98,11 +58,6 @@ if __name__ == "__main__":
     
     estimate_df = apply_estimation(**config)
 
-    # cell_no frotover exist in both dfs we load twice finalsel
-    # cell_no_x is original no changes, cell_no_y from estimation (some changes applied for NI and UK)
-    # frotover_x has na for derived values frotover_y has original values
-    
-    
     estimate_df = estimate_df.drop(columns = ["cell_no","frotover"])
 
     post_estimate = pd.merge(post_constrain,estimate_df,how = "left",on = ["period","reference"])
@@ -111,8 +66,10 @@ if __name__ == "__main__":
         
     estimate_out.to_csv(config['out_path']+f"estimation_output_{FILE_VERSION}.csv",index=False)
                 
-    post_win = wrap_winsorised(post_estimate,config['l_values_path'])
+    post_win = winsorise(post_estimate,config['l_values_path'])
     
     post_win.to_csv(config['out_path']+f"winsorisation_output_{FILE_VERSION}.csv",index=False)
 
     asap_482_df = get_qa_output_482(post_win)
+    
+    asap_482_df.to_csv(config['out_path']+f"asap_482_df{FILE_VERSION}.csv",index=False)
