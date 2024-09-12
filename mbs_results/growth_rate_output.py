e7d9f4c09d84d7bbd2f8a41bb54ed5500b904445
daviel9 <Luke.Davies@ons.gov.uk>
2024-09-12 13:42:57 +0100

    Rename columns to match new input data

--- mbs_results/growth_rate_output.py
+++ mbs_results/growth_rate_output.py
@@ -24,41 +24,39 @@ def get_growth_rate_data(filepath: str) -> pd.DataFrame:
         usecols=[
             "classification",
             "question_no",
-            "cell_no_x",
-            "period_x",
-            "weighted_adjusted_value",
+            "cell_no",
+            "period",
+            "weighted adjusted value",
         ],
         dtype={
             "classification": "Int32",
             "question_no": "Int8",
-            "cell_no_x": "Int16",
-            "period_x": "Int32",
-            "weighted_adjusted_value": "float64",
+            "cell_no": "Int16",
+            "period": "Int32",
+            "weighted adjusted value": "float64",
         },
     )
 
-    input_data["period_x"] = (
-        convert_column_to_datetime(input_data["period_x"])
-        .dt.strftime("%Y%b")
-        .str.upper()
+    input_data["period"] = (
+        convert_column_to_datetime(input_data["period"]).dt.strftime("%Y%b").str.upper()
     )
 
     input_data["sizeband"] = np.where(
-        input_data["cell_no_x"].isna(),
-        input_data["cell_no_x"],
-        input_data.cell_no_x.astype(str).str[-1],
+        input_data["cell_no"].isna(),
+        input_data["cell_no"],
+        input_data.cell_no.astype(str).str[-1],
     )
 
-    input_data.drop(columns=["cell_no_x"], inplace=True)
+    input_data.drop(columns=["cell_no"], inplace=True)
 
     input_data.sort_values(
-        ["classification", "question_no", "sizeband", "period_x"], inplace=True
+        ["classification", "question_no", "sizeband", "period"], inplace=True
     )
 
     growth_rate_output = (
         input_data.pivot_table(
-            columns="period_x",
-            values="adjusted_value",
+            columns="period",
+            values="weighted adjusted value",
             index=["classification", "question_no", "sizeband"],
             aggfunc="sum",
             dropna=False,
