9a5142a62c500b005b1fd2b608a57515c38e8c1f
Jordan Day <jordan.day@ons.gov.uk>
2024-09-23 12:56:24 +0100

    Update to scottish outputs files

--- mbs_results/scottish_outputs.py
+++ mbs_results/scottish_outputs.py
@@ -1,25 +1,64 @@
-loaded_config = {"scottish_outputs": [
-'period', 'SUT', 'cell', 'RU', 'name', 'enterprise group',
-'SIC', 'form type', 'status', '%scottish', 'frozen employment',
-'band', 'response type', 'error marker', 'start date', 'end date', 
-'returned turnover', 'adjusted turnover', 'response type.1', 'error marker.1',
-'returned to exports', 'adjused to exports', 'response type.2', 'error marker.2',
-'returned total employment', 'adjusted total employment', 'response type.3', 'error marker.3', 
-'returned FTM', 'adjusted FTM', 'response type.4', 'error marker.4', 
-'returned PTM', 'adjusted PTM', 'response type.5', 'error marker.5', 
-'returned FTF', 'adjusted FTF', 'response type.6', 'error marker.6', 
-'returned PTF', 'adjusted PTF', 'response type.7', 'error marker.7', 
-'returned volume water', 'adjusted volume water', 'response type.8', 'error marker.8'
-]}
+loaded_config = {
+    "scottish_outputs": [
+        "period",
+        "SUT",
+        "cell",
+        "RU",
+        "name",
+        "enterprise group",
+        "SIC",
+        "form type",
+        "status",
+        "%scottish",
+        "frozen employment",
+        "band",
+        "response type",
+        "error marker",
+        "start date",
+        "end date",
+        "returned turnover",
+        "adjusted turnover",
+        "response type.1",
+        "error marker.1",
+        "returned to exports",
+        "adjused to exports",
+        "response type.2",
+        "error marker.2",
+        "returned total employment",
+        "adjusted total employment",
+        "response type.3",
+        "error marker.3",
+        "returned FTM",
+        "adjusted FTM",
+        "response type.4",
+        "error marker.4",
+        "returned PTM",
+        "adjusted PTM",
+        "response type.5",
+        "error marker.5",
+        "returned FTF",
+        "adjusted FTF",
+        "response type.6",
+        "error marker.6",
+        "returned PTF",
+        "adjusted PTF",
+        "response type.7",
+        "error marker.7",
+        "returned volume water",
+        "adjusted volume water",
+        "response type.8",
+        "error marker.8",
+    ]
+}
 
-import pandas as pd 
+import pandas as pd
 
 df = pd.read_csv("d:/temp_outputs_new_env/winsorisation_output_0.0.2.csv")
-df= df.columns.to_list()
+df = df.columns.to_list()
 print(df)
 
 
-def scottish_outputs(df:pd.DataFrame, scotish_columns:list,sup_data:pd.DataFrame):
+def scottish_outputs(df: pd.DataFrame, scotish_columns: list, sup_data: pd.DataFrame):
     """
     Function to produce Scottish (and Welsh?) outputs
     Some data is not available from only MBS, do we need to request QBS data,
@@ -33,7 +72,5 @@ def scottish_outputs(df:pd.DataFrame, scotish_columns:list,sup_data:pd.DataFrame
         _description_
     sup_data : pd.DataFrame
         _description_
-    """    
+    """
     df[scotish_columns]
-
-
