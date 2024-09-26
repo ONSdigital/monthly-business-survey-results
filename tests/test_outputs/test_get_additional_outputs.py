9b014a01c91c06c1aa2dc7f1f1dad5ca59607933
zogkoa <Anton.Zogkolli@ons.gov.uk>
2024-09-26 14:31:21 +0100

    Run hooks

--- tests/test_outputs/test_get_additional_outputs.py
+++ tests/test_outputs/test_get_additional_outputs.py
@@ -2,33 +2,39 @@ import pytest
 
 from mbs_results.outputs.get_additional_outputs import get_additional_outputs
 
-def test1(**kwargs):print(1) #dummy function
-def test2(**kwargs):print(2) #dummy function
+
+def test1(**kwargs):
+    print(1)  # dummy function
+
+
+def test2(**kwargs):
+    print(2)  # dummy function
+
 
 @pytest.fixture(scope="class")
 def function_mapper():
-    return  {
-    "test1" : test1,
-    "test2" : test2
-   }
+    return {"test1": test1, "test2": test2}
 
-@pytest.mark.parametrize('inp, expected', [
-    ({"additional_outputs": ["all"]}, '1\n2\n'),
-    ({"additional_outputs": ["test1"]}, '1\n'),
-    ({"additional_outputs": ["test2"]}, '2\n'),
-    ({"additional_outputs": []}, 'No additional_outputs produced\n')
-])
 
-def test_output(capsys,function_mapper, inp, expected):
-    get_additional_outputs(inp,function_mapper)
+@pytest.mark.parametrize(
+    "inp, expected",
+    [
+        ({"additional_outputs": ["all"]}, "1\n2\n"),
+        ({"additional_outputs": ["test1"]}, "1\n"),
+        ({"additional_outputs": ["test2"]}, "2\n"),
+        ({"additional_outputs": []}, "No additional_outputs produced\n"),
+    ],
+)
+def test_output(capsys, function_mapper, inp, expected):
+    get_additional_outputs(inp, function_mapper)
     out, err = capsys.readouterr()
     assert out == expected
 
+
 def test_raise_errors(function_mapper):
-    
-    with pytest.raises(ValueError):
-        get_additional_outputs({"additional_outputs": "not_a_list"},function_mapper)
 
     with pytest.raises(ValueError):
-        get_additional_outputs( {"additional_outputs": ["test3"]},function_mapper)
+        get_additional_outputs({"additional_outputs": "not_a_list"}, function_mapper)
 
+    with pytest.raises(ValueError):
+        get_additional_outputs({"additional_outputs": ["test3"]}, function_mapper)
