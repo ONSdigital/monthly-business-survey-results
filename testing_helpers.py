9c6d585383b76768d1b6917884727ecaf488786a
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2024-10-07 11:26:26 +0100

    Combine adjusted and imputed values to one column (#94)

--- testing_helpers.py
+++ testing_helpers.py
@@ -135,13 +135,13 @@ def get_qa_output_482(post_win_df):
         'default_link_f_match_adjusted_value','b_link_adjusted_value',
         'b_match_adjusted_value_pair_count','default_link_b_match_adjusted_value',
         'construction_link', 'flag_construction_matches_pair_count','default_link_flag_construction_matches',
-        "imputed_value","constrain_marker" # these not requested but usefull
+        "constrain_marker" # these not requested but usefull
         ]
 
     # not part of the pipeline the below
     post_win_df['total weight (A*G*O)'] = post_win_df['design_weight']*post_win_df['calibration_factor']*post_win_df['outlier_weight']
     
-    post_win_df['weighted adjusted value'] =post_win_df['imputed_value'] * post_win_df['total weight (A*G*O)']
+    post_win_df['weighted adjusted value'] =post_win_df['adjusted_value'] * post_win_df['total weight (A*G*O)']
 
 
     return post_win_df[requested_columns]
