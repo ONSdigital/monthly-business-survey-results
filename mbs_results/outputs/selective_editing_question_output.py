0c5260277039ceeecaecba438b506ef43b4d73ab
Jordan Day <jordan.day@ons.gov.uk>
2024-10-08 14:42:37 +0100

    Update to output and create validation function

--- mbs_results/outputs/selective_editing_question_output.py
+++ mbs_results/outputs/selective_editing_question_output.py
@@ -11,21 +11,21 @@ from mbs_results.unsorted.selective_editing import (
 
 def create_selective_editing_question_output(
     df: pd.DataFrame,
-    reference ,
+    reference,
     period,
     domain,
     question_no,
-    sic ,
+    sic,
     aux: str,
     a_weight: str,
     o_weight: str,
     g_weight: str,
     predicted_value: str,
-    imputed_value ,
+    imputed_value,
     adjusted_value: str,
-    output_path: str, 
-    version: str, 
-    previous_period: int
+    output_path: str,
+    version: str,
+    previous_period: int,
 ):
     """
     creates and saves the selective editing question output to the output_path folder
@@ -48,15 +48,21 @@ def create_selective_editing_question_output(
     sic_domain_mapping = pd.read_csv(
         output_path + "mapping_files/sic_domain_mapping.csv"
     ).astype(int)
-    df_with_domain = merge_domain(input_df=df, 
-                                  domain_mapping=sic_domain_mapping, 
-                                  sic_input=sic, 
-                                  sic_mapping="sic_5_digit")
-    df_with_domain = calculate_predicted_value(
-        dataframe=df_with_domain, imputed_value=imputed_value, adjusted_value=adjusted_value
+
+    df_with_domain = merge_domain(
+        input_df=df,
+        domain_mapping=sic_domain_mapping,
+        sic_input=sic,
+        sic_mapping="sic_5_digit",
     )
+    df_with_domain = calculate_predicted_value(
+        dataframe=df_with_domain,
+        imputed_value=imputed_value,
+        adjusted_value=adjusted_value,
+    )
+
     standardising_factor = create_standardising_factor(
-        dataframe = df_with_domain,
+        dataframe=df_with_domain,
         reference=reference,
         period=period,
         domain=domain,
@@ -69,16 +75,34 @@ def create_selective_editing_question_output(
         auxiliary_value=aux,
         previous_period=previous_period,
     )
+
     standardising_factor["survey_code"] = "009"
-    save_output = False
-    if save_output:
-        standardising_factor.to_csv(
-            output_path
-            + "selective_editing_outputs/"
-            + f"selective_editing_question_output_{previous_period}_{version}.csv",
-            index=False,
-        )
+
+    # TODO Update file name
+    standardising_factor.to_csv(
+        output_path
+        + "selective_editing_outputs/"
+        + f"selective_editing_question_output_{previous_period}_{version}.csv",
+        index=False,
+    )
     return standardising_factor
+
+def validation_checks_selective_editing(df:pd.DataFrame):
+    """
+    validation checks for duplicate rows and number of NaNs
+    Currently prints to console.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        dataframe output from `create_selective_editing_question_output`
+    """    
+    print('Number of duplicates, (checking period, question_no, and reference:',
+    df.duplicated(subset=['period',"question_no","reference"]).sum())
+    predicted_na = df.loc[df["predicted_value"].isna()]
+    number_nans = predicted_na.count()[0]
+    print(f"predicted_values has {number_nans} NaNs")
+
 
 
 if __name__ == "__main__":
@@ -91,45 +115,22 @@ if __name__ == "__main__":
         output_path + f"winsorisation/winsorisation_output_{version}.csv"
     )
 
-
-    out = create_selective_editing_question_output(df=wins_output,
-                                                   reference="reference",
-                                                   period="period",
-                                                   domain="domain", 
-                                                   question_no="question_no",
-                                                   sic = "sic_5_digit", 
-                                                   aux="frotover",
-                                                   a_weight="design_weight",
-                                                   o_weight = "outlier_weight",
-                                                   g_weight = "calibration_factor",
-                                                   predicted_value= "predicted_value",
-                                                   imputed_value="imputed_value", 
-                                                   adjusted_value="adjusted_value", 
-                                                   output_path=output_path,
-                                                   version=version,
-                                                   previous_period=202302)
-
-    print(out)
-
-    # print('Number of duplicates, (checking period, question_no, and reference:',
-    #        saved_version.duplicated(subset=['period',"question_no","reference"]).sum())
-    # predicted_na = saved_version.loc[saved_version["predicted_value"].isna()]
-    # number_nans = predicted_na.count()[0]
-    # reference_no_list = predicted_na["reference"].unique().tolist()
-    # print(f"predicted_values has {number_nans} NaNs")
-    # print(wins_output.loc[(wins_output["reference"].isin(reference_no_list))
-    #                       & (wins_output["question_no"] == 40)
-    #                       & (wins_output["period"] == 202302)])
-
-    # df = df_with_domain,
-    #     reference=reference,
-    #     period=period,
-    #     domain=domain,
-    #     question_no=question_no,
-    #     predicted_value="predicted_value",
-    #     imputation_marker="imputation_flags_adjusted_value",
-    #     a_weight="design_weight",
-    #     o_weight="outlier_weight",
-    #     g_weight="calibration_factor",
-    #     auxiliary_value="frotover",
-    #     previous_period=previous_period,
+    output = create_selective_editing_question_output(
+        df=wins_output,
+        reference="reference",
+        period="period",
+        domain="domain",
+        question_no="question_no",
+        sic="sic_5_digit",
+        aux="frotover",
+        a_weight="design_weight",
+        o_weight="outlier_weight",
+        g_weight="calibration_factor",
+        predicted_value="predicted_value",
+        imputed_value="imputed_value",
+        adjusted_value="adjusted_value",
+        output_path=output_path,
+        version=version,
+        previous_period=202302,
+    )
+    validation_checks_selective_editing(output)
