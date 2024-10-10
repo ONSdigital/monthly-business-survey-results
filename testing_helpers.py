99c0e5639affe59c1efb4ceb2bd681627710625c
BixLaw <bix.lawrence@ons.gov.uk>
2024-10-10 15:21:22 +0100

    updating froempees to froempment

--- testing_helpers.py
+++ testing_helpers.py
@@ -127,15 +127,35 @@ def get_qa_output_482(post_win_df):
     """For ASAP-482"""
     
     requested_columns = [
-        'reference',"period","frosic2007",'classification','cell_no','frotover',
-        'froempees', 'form_type', 'response_type', 'question_no','adjusted_value',
-        'error_mkr', 'design_weight','calibration_factor', 'outlier_weight',
-        'total weight (A*G*O)','weighted adjusted value', 'imputation_flags_adjusted_value',
-        'imputation_class','f_link_adjusted_value', 'f_match_adjusted_value_pair_count',
-        'default_link_f_match_adjusted_value','b_link_adjusted_value',
-        'b_match_adjusted_value_pair_count','default_link_b_match_adjusted_value',
-        'construction_link', 'flag_construction_matches_pair_count','default_link_flag_construction_matches',
-        "constrain_marker" # these not requested but usefull
+        'reference',
+        'period',
+        'frosic2007',
+        'classification',
+        'cell_no',
+        'frotover',
+        'froempment', 
+        'form_type', 
+        'response_type', 
+        'question_no',
+        'adjusted_value',
+        'error_mkr', 
+        'design_weight',
+        'calibration_factor', 
+        'outlier_weight',
+        'total weight (A*G*O)',
+        'weighted adjusted value', 
+        'imputation_flags_adjusted_value',
+        'imputation_class',
+        'f_link_adjusted_value', 
+        'f_match_adjusted_value_pair_count',
+        'default_link_f_match_adjusted_value',
+        'b_link_adjusted_value',
+        'b_match_adjusted_value_pair_count',
+        'default_link_b_match_adjusted_value',
+        'construction_link', 
+        'flag_construction_matches_pair_count',
+        'default_link_flag_construction_matches',
+        'constrain_marker' # these not requested but usefull
         ]
 
     # not part of the pipeline the below
