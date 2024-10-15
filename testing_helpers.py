eecd18c00ca555fe37879e5ae9efe45916678db8
Zogkolli <Anton.Zogkolli@ons.gov.uk>
2024-10-15 12:40:23 +0100

    Update impute rules

--- testing_helpers.py
+++ testing_helpers.py
@@ -59,16 +59,12 @@ def proccess_for_pre_impute(df):
     # Updating response_type to 1 for 15399057545 , this is a known error check ASAP-492 for details
     df.loc[df['reference']==15399057545,"response_type"] = 1
     
-    zero_to_null_rules = (
-         ((df["adjusted_value"]==0) 
-         & ((df["response_type"]>=4)  #response_type >=4 leave 0 
-        | ((df["response_type"]==2) & (df["type"]==1)) # or response_type 2 and type 1 leave 0
-            )) & (df["period"]!=df["period"].min()) # Avoiding overwriting first period records as this will cause some business not to forward impute
-        )
-    
-    convert_to_null_rules = (
-        ((df["response_type"]==1) & (df["type"]==5)) #response_type 1 type 5 remove values
-        )
+    imputed_values_rules = (
+        (df["type"].isin([3, 4, 5, 6])) & 
+        (df["period"] != df["period"].min())
+    )
+
+    df.loc[imputed_values_rules, "adjusted_value"] = np.nan
     
     df["form_type"] = df["form_type"].str.strip() #remove whitespace
 
@@ -81,24 +77,17 @@ def proccess_for_pre_impute(df):
     
     df.drop(df[remove_derived_rules].index, inplace=True)
     
-    df.loc[zero_to_null_rules,"adjusted_value"] = np.nan
-    df.loc[convert_to_null_rules,"adjusted_value"] = np.nan
-
     df.reset_index(drop=True,inplace=True)
 
     #Some checks to check if rules were applied
-    print("The below must not have values for type 2 since we dropped them\n",
-          df.groupby(["response_type","type"])["adjusted_value"].count()
-          )
-    
-    print("Selecting all the nas and zeros of adjusted value\n",
-        "The below must not have 0 for response_type 2 type 1 and response_type >=4\n",
-        "Also for response type 1 and type 5 must have only nans (mannual costructed)\n",
-        df.loc[df["adjusted_value"].isin([np.nan,0])].groupby(["response_type","type"])["adjusted_value"].unique())
+    nan_and_zero_check = df.loc[df["adjusted_value"].isna() | (df["adjusted_value"] == 0)]
+
+    print("Verification after applying NaN rules on 'adjusted_value':")
+    print("1. 'adjusted_value' set to NaN for type 3, 4, 5, 6, excluding first period:")
+    print("   - All NaN entries by 'response_type' and 'type':")
+    print(nan_and_zero_check.groupby(["response_type", "type"])["adjusted_value"].unique())
     
     return df
-        
-
     
 def check_na_duplicates(df):
     """Does basic checks"""
@@ -198,7 +187,7 @@ def join_l_values(df,l_values_path, classification_values_path):
 def extract_mannual_constructed(df):
     """Get data which were mannual constructed"""
     
-    df_man = df.loc[((df["response_type"]==1) & (df["type"]==5))]
+    df_man = df.loc[(df["type"]==5)]
     
     df_man = df_man[['period', 'reference', 'question_no','adjusted_value']]
     
