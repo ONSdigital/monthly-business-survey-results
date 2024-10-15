dab00cbe74419052faaec7d677fa66c496509f19
Zogkolli <Anton.Zogkolli@ons.gov.uk>
2024-10-15 12:56:06 +0100

    Update modules import for new mbs folder structure

--- testing_main.py
+++ testing_main.py
@@ -1,6 +1,5 @@
 from importlib import metadata
 import pandas as pd
-
 from testing_helpers import (
     get_pre_impute_data, proccess_for_pre_impute,
     check_na_duplicates,map_form_type,get_qa_output_482,
@@ -9,11 +8,11 @@ from testing_helpers import (
 
 # pip install git+https://github.com/ONSdigital/monthly-business-survey-results.git@0.0.2
 
-from mbs_results.data_cleaning import create_imputation_class
-from mbs_results.ratio_of_means import ratio_of_means
-from mbs_results.constrains import constrain
+from mbs_results.utilities.data_cleaning import create_imputation_class
+from mbs_results.imputation.ratio_of_means import ratio_of_means
+from mbs_results.unsorted.constrains import constrain
 from mbs_results.apply_estimation import apply_estimation
-from mbs_results.winsorisation import winsorise
+from mbs_results.outlier_detection.winsorisation import winsorise
 
     
 if __name__ == "__main__":
