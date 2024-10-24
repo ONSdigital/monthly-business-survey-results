576c5aebf07077bc79e1522772aca1415526fd40
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2024-10-24 14:42:26 +0100

    632 module restructure (#116)
    * move files to correct location
    
    * change relevant module imports
    
    * Move files to apropiate folders, rename folders
    
    * Move all data to equivalent test folder structure
    
    * Update estimation test paths and imports
    
    * Update imputation test paths and imports
    
    * Update outlier detection test paths and imports
    
    * Update outpus test paths and imports
    
    * Update utilities test paths and imports
    
    * Remove tests/imputation/test_pivot_imputation_value.py
    
    * Run hooks
    
    * Add tests tree into tests readme
    
    * Run hooks
    
    * Remove duplicated test data
    
    * These were copied instead of moved, hence duplicated
    
    * these files aren't needed and covered by other tests
    
    * update tree
    
    ---------
    
    Co-authored-by: Wil Roberts <Wil.Roberts@ons.gov.uk>

---
+++ tests/outlier_detection/test_calculate_winsorised_weight.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from pandas.testing import assert_frame_equal
+
+from mbs_results.outlier_detection.calculate_winsorised_weight import (
+    calculate_winsorised_weight,
+)
+
+
+@pytest.fixture(scope="class")
+def winsorised_weight_test_data():
+    return pd.read_csv(
+        Path("tests")
+        / "data"
+        / "outlier_detection"
+        / "calculate_winsorised_weight"
+        / "winsorised_weight_data.csv",
+        low_memory=False,
+        usecols=lambda c: not c.startswith("Unnamed:"),
+    )
+
+
+@pytest.fixture(scope="class")
+def winsorised_weight_test_output():
+    return pd.read_csv(
+        Path("tests")
+        / "data"
+        / "outlier_detection"
+        / "calculate_winsorised_weight"
+        / "winsorised_weight_data_output.csv",
+        low_memory=False,
+        usecols=lambda c: not c.startswith("Unnamed:"),
+    )
+
+
+class TestWinsorisedWeight:
+    def test_winsorised_weight(
+        self, winsorised_weight_test_output, winsorised_weight_test_data
+    ):
+        expected_output = winsorised_weight_test_output[
+            [
+                "group",
+                "period",
+                "aux",
+                "sampled",
+                "a_weight",
+                "g_weight",
+                "target_variable",
+                "predicted_unit_value",
+                "l_value",
+                "ratio_estimation_treshold",
+                "nw_ag_flag",
+                "new_target_variable",
+                "outlier_weight",
+            ]
+        ]
+        input_data = winsorised_weight_test_data[
+            [
+                "group",
+                "period",
+                "aux",
+                "sampled",
+                "a_weight",
+                "g_weight",
+                "target_variable",
+                "predicted_unit_value",
+                "l_value",
+                "ratio_estimation_treshold",
+                "nw_ag_flag",
+            ]
+        ]
+
+        actual_output = calculate_winsorised_weight(
+            input_data,
+            "group",
+            "period",
+            "aux",
+            "sampled",
+            "a_weight",
+            "g_weight",
+            "target_variable",
+            "predicted_unit_value",
+            "l_value",
+            "ratio_estimation_treshold",
+            "nw_ag_flag",
+        )
+
+        assert_frame_equal(actual_output, expected_output)
