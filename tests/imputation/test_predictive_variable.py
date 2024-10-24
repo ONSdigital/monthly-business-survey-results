b33f980b101daa96d4c9a76704eb2d967c42481d
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2024-10-24 14:42:26 +0100

    632 module restructure (#116)
    * move files to correct location
    
    * change relevant module imports
    
    * Move files to apropiate folders, rename folders
    
    * Move all data to equivalent test folder structure
    
    * Update estimation test paths and imports
    
    * Update imputation test paths and imports
    
    * Update outlier detection test paths and imports
    
    * Update outpus test paths and imports
    
    * Update utilities test paths and imports
    
    * Remove tests/imputation/test_pivot_imputation_value.py
    
    * Run hooks
    
    * Add tests tree into tests readme
    
    * Run hooks
    
    * Remove duplicated test data
    
    * These were copied instead of moved, hence duplicated
    
    * these files aren't needed and covered by other tests
    
    * update tree
    
    ---------
    
    Co-authored-by: Wil Roberts <Wil.Roberts@ons.gov.uk>

---
+++ tests/imputation/test_predictive_variable.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+from pandas.testing import assert_frame_equal
+
+from mbs_results.imputation.predictive_variable import shift_by_strata_period
+from tests.helper_functions import load_and_format
+
+
+@pytest.fixture(scope="class")
+def predictive_variable_test_data():
+    return load_and_format(
+        Path("tests/data/imputation/predictive_variable") / "predictive_variable.csv"
+    )
+
+
+class TestPredictiveVariable:
+    def test_predictive_variable_forward(self, predictive_variable_test_data):
+        expected_output = predictive_variable_test_data[
+            ["identifier", "period", "group", "question", "other", "f_predictive"]
+        ]
+        input_data = expected_output.drop(columns="f_predictive")
+        actual_output = shift_by_strata_period(
+            input_data, "question", "period", "group", "identifier", 1, "f_predictive"
+        )
+        actual_output.sort_index(ascending=True, inplace=True)
+        assert_frame_equal(actual_output, expected_output)
+
+    def test_predictive_variable_backward(self, predictive_variable_test_data):
+
+        expected_output = predictive_variable_test_data[
+            ["identifier", "period", "group", "question", "other", "b_predictive"]
+        ]
+        input_data = expected_output.drop(columns="b_predictive")
+        actual_output = shift_by_strata_period(
+            input_data, "question", "period", "group", "identifier", -1, "b_predictive"
+        )
+        actual_output.sort_index(ascending=True, inplace=True)
+        assert_frame_equal(actual_output, expected_output)
