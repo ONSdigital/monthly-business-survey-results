088cde701cc222b879952de2869dfacac4d1c32f
Jordan Day <jordan.day@ons.gov.uk>
2024-10-24 12:32:55 +0100

    staging main commit hooks

--- mbs_results/staging/staging_main.py
+++ mbs_results/staging/staging_main.py
@@ -1,7 +1,9 @@
 from mbs_results.staging.dfs_from_spp import dfs_from_spp
-from mbs_results.inputs import load_config
+from mbs_results.utilities.inputs import load_config
 
 config = load_config()
 contributors, responses = dfs_from_spp(
-  config["client"], config["bucket_name"], config["filepath"], config["platform"]
+    config["folder_path"] + config["mbs_file_name"],
+    config["platform"],
+    config["bucket"],
 )
