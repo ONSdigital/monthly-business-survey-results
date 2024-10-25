5b357a232db1db43bc97672272de446d565a64b5
Jordan Day <jordan.day@ons.gov.uk>
2024-10-25 10:17:10 +0100

    Chnages to stage_dataframe. Function is broken and uses high memeory when performing joins

--- mbs_results/staging/stage_dataframe.py
+++ mbs_results/staging/stage_dataframe.py
@@ -1,9 +1,14 @@
+import warnings
+
 import pandas as pd
 
 from mbs_results.staging.convert_ni_uk import convert_ni_uk
 from mbs_results.staging.create_missing_questions import create_missing_questions
 from mbs_results.staging.dfs_from_spp import dfs_from_spp
-from mbs_results.utilities.utils import read_colon_separated_file
+from mbs_results.utilities.utils import (
+    convert_column_to_datetime,
+    read_colon_separated_file,
+)
 
 
 def create_mapper() -> dict:
@@ -16,6 +21,7 @@ def create_mapper() -> dict:
         dictionary containing question numebers and form id
     """
     mapper = {9: [40, 49], 10: [110]}
+    warnings.warn("create_mapper is a placeholder function and needs to be defined")
     return mapper
 
 
@@ -39,6 +45,12 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
         *** current functionality broken but with refactoring this will be implemented
         Mapping function needs to be defined but is used in other functions
     """
+
+    warnings.warn("Issues with memory when joining datasets occur in this function")
+    print("Staging started")
+    period = config["period"]
+    reference = config["reference"]
+
     contributors, responses = dfs_from_spp(
         config["folder_path"] + config["mbs_file_name"],
         config["platform"],
@@ -46,23 +58,33 @@ def stage_dataframe(config: dict) -> pd.DataFrame:
     )
     # contributors = enforce_datatypes(df, **config) #BROKEN
     # responses = enforce_datatypes(df, **config) #BROKEN
-    finalsel = read_colon_separated_file("sample_path", config["sample_column_names"])
-    contributors_final_sel = pd.merge(
-        left=contributors, right=finalsel, left_on="", right_on=""
+    finalsel = read_colon_separated_file(
+        config["sample_path"], config["sample_column_names"], period=config[period]
     )
+
+    # Temp convert d types while enforce datatypes is broken
+    finalsel[period] = convert_column_to_datetime(finalsel[period])
+    finalsel[reference] = finalsel[period].astype(str)
+    contributors[period] = convert_column_to_datetime(contributors[period])
+    contributors[reference] = contributors[period].astype(str)
+    responses[period] = convert_column_to_datetime(responses[period])
+    responses[reference] = responses[period].astype(str)
+
+    # MEMORY ISSUES WITH JOIN?
+    contributors = pd.merge(left=contributors, right=finalsel, on=[period, reference])
+
     mapper = create_mapper()  # Needs to be defined
     responses_with_missing = create_missing_questions(
-        contributors_df=contributors_final_sel,
+        contributors_df=contributors,
         responses_df=responses,
-        reference=config["reference"],
-        period=config["period"],
+        reference=reference,
+        period=period,
         formid=config["form_id"],
         question_no=config["question_no"],
         mapper=mapper,
     )
-    df = responses_with_missing.merge(
-        contributors_final_sel, on=[config["reference"], config["period"]]
-    )
+    df = responses_with_missing.merge(contributors, on=[reference, period])
     df = convert_ni_uk(df, "cellnumber")
+    print("Staging Completed")
 
     return df
