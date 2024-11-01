e6d9e66cf902cc6c960e011bee9b723e14bd8d16
Jordan Day <jordan.day@ons.gov.uk>
2024-11-01 12:33:41 +0000

    added where correct_values should be called.

--- mbs_results/estimation/estimate.py
+++ mbs_results/estimation/estimate.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 from mbs_results.estimation.apply_estimation import apply_estimation
 
+# from mbs_results.staging.data_cleaning import correct_values
+
 
 def estimate(df: pd.DataFrame, config):
     """
@@ -27,6 +29,7 @@ def estimate(df: pd.DataFrame, config):
     )
     config["calibration_group_map"] = pd.read_csv(config["calibration_group_map"])
     estimate_df = apply_estimation(**config)
+    # estimate_df = correct_values()
     estimate_df = estimate_df.drop(columns=["cell_no", "frotover", "frosic2007"])
     post_estimate = pd.merge(df, estimate_df, how="left", on=["period", "reference"])
 
