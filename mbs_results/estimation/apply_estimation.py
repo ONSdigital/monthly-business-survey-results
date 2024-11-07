a5303b553b63a2a97565ce6ece483b693057bb1d
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2024-11-07 12:14:13 +0000

    498 sampled_flag is providing flag for census groups (#122)
    * Feat is_cencus function
    
    *Check if calibration group is cencus
    *Add unit test, test data
    *Remove correct_values function,test,test data
    *Implement is_cencus in apply estimation
    
    * Fix is_cencus input args
    
    * Add extra cal groups as function argument and in config
    
    * Add __init__.py to staging 
    
    Is essential for the staging to be packaged
    
    * Add extra calibration groups as apply_estimation arg

--- mbs_results/estimation/apply_estimation.py
+++ mbs_results/estimation/apply_estimation.py
@@ -7,11 +7,19 @@ from mbs_results.estimation.calculate_estimation_weights import (
     calculate_design_weight,
 )
 from mbs_results.estimation.pre_processing_estimation import get_estimation_data
+from mbs_results.staging.data_cleaning import is_census
 
 # from mbs_results.estimation.validate_estimation import validate_estimation
 
 
-def apply_estimation(population_path, sample_path, period, **config):
+def apply_estimation(
+    population_path,
+    sample_path,
+    calibration_group,
+    census_extra_calibration_group,
+    period,
+    **config
+):
     """
     Read population frame and sample, merge key variables onto df then derive
     and validate estimation weights.
@@ -22,6 +30,10 @@ def apply_estimation(population_path, sample_path, period, **config):
         filepath for population frame data
     sample_path : str
         filepath for sample data
+    calibration_group: str
+        column name of dimension contaning calibration group values
+    census_extra_calibration_group: list
+        calibration groups which are census but not band 4 or 5
     period : str
         name of column containing period
 
@@ -45,12 +57,30 @@ def apply_estimation(population_path, sample_path, period, **config):
             population_file, sample_file, period, **config
         )
 
-        estimation_data = calculate_design_weight(estimation_data, period, **config)
-        estimation_data = calculate_calibration_factor(
-            estimation_data, period, **config
-        )
+        census_df = estimation_data[
+            is_census(
+                estimation_data[calibration_group], census_extra_calibration_group
+            )
+        ]
 
-        estimation_df_list.append(estimation_data)
+        census_df["design_weight"] = 1
+        census_df["calibration_factor"] = 1
+        census_df["sampled"] = 0
+
+        non_census_df = estimation_data[
+            ~(
+                is_census(
+                    estimation_data[calibration_group], census_extra_calibration_group
+                )
+            )
+        ]
+
+        non_census_df = calculate_design_weight(non_census_df, period, **config)
+        non_census_df = calculate_calibration_factor(non_census_df, period, **config)
+
+        all_together = pd.concat([non_census_df, census_df], ignore_index=True)
+
+        estimation_df_list.append(all_together)
 
     estimation_df = pd.concat(estimation_df_list, ignore_index=True)
 
