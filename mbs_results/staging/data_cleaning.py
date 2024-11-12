090311a3d77328cf3cc977edb809a99e6067cc2e
Jordan Day <jordan.day@ons.gov.uk>
2024-11-12 15:33:30 +0000

    Removed filling na's with an empty string.
    - empty string was causing issues by changing datatype.
    - removed final line filling all nans with string.

--- mbs_results/staging/data_cleaning.py
+++ mbs_results/staging/data_cleaning.py
@@ -321,9 +321,9 @@ def run_live_or_frozen(
 
     if state == "frozen":
         df["frozen_error"] = df.apply(
-            lambda x: x[target] if x[error_marker] in (error_values) else "", axis=1
+            lambda x: x[target] if x[error_marker] in (error_values) else None, axis=1
         )
-        df = df.fillna("")
+        #df = df.fillna("")
 
     return df
 
