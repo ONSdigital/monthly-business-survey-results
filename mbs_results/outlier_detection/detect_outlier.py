d2eee208e88d977b2d4c4f5095dbcb74e0ee9ff4
Anton Zogkolli <Anton.Zogkolli@ons.gov.uk>
2024-11-19 16:57:45 +0000

    Fix hard coded question_no column

--- mbs_results/outlier_detection/detect_outlier.py
+++ mbs_results/outlier_detection/detect_outlier.py
@@ -49,7 +49,7 @@ def detect_outlier(df, config):
         df, config["l_values_path"], config["classification_values_path"], config
     )
 
-    post_win = pre_win.groupby("question_no").apply(
+    post_win = pre_win.groupby(config["question_no"]).apply(
         lambda df: winsorise(
             df,
             "calibration_group",
