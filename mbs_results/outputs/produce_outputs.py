be27eafa49dfe6dd985b88cd4ae3219ecbf67e57
Luke Hubbard <104831245+lhubbardONS@users.noreply.github.com>
2024-12-12 08:16:37 +0000

    Adding additional outputs wrapper (#126)
    * Adding additional outputs wrapper and making edits to additional outputs scripts to work correctly with wrapper.
    
    * Streamlining additional outputs process and updating methods to work correctly with pipeline
    
    * pre-commit hooks

--- mbs_results/outputs/produce_outputs.py
+++
@@ -1,7 +0,0 @@
-import warnings
-
-import pandas as pd
-
-
-def produce_outputs(df: pd.DataFrame, output_path: str):
-    warnings.warn("Temporary function to produce optional outputs")
--- mbs_results/outputs/turnover_analysis.py
+++ mbs_results/outputs/turnover_analysis.py
@@ -3,33 +3,19 @@ import pandas as pd
 
 
 def create_turnover_output(
-    cp_df: pd.DataFrame,
-    qv_df: pd.DataFrame,
-    finalsel_df: pd.DataFrame,
-    winsorisation_df: pd.DataFrame,
-    winsorisation_period: str,
-    selected_period: int,
+    additional_outputs_df: pd.DataFrame, current_period: int, **config
 ) -> pd.DataFrame:
     """
     Creating output for turnover analysis tool.
 
     Parameters
     ----------
-    cp_df : pd.DataFrame
-        cp input dataframe containing reference, sic and error_mkr
-    qv_df : pd.DataFrame
-        qv input dataframe containing reference, question_no, adjusted_value and
-        returned_value
-    finalsel_df : pd.DataFrame
-        finalsel input dataframe containing reference, froempment, frotover, cell_no
-        and entname1
-    winsorisation_df : pd.DataFrame
-        winsorisation input dataframe containing question_no, period, reference,
-        imputation_marker, design_weight, calibration_factor and outlier_weight
-    winsorisation_period : str
-        Name of column displaying period in winsorisation
-    selected_period : int
+    additional_outputs_df : pd.DataFrame
+        estimation input dataframe containing relevant columns for turnover tool
+    current_period : int
         Period to output results for in the format YYYYMM
+    **config: Dict
+          main pipeline configuration. Can be used to input the entire config dictionary
 
     Returns
     -------
@@ -37,19 +23,12 @@ def create_turnover_output(
         dataframe in correct format for populating turnover analysis tool.
     """
 
-    qv_df = qv_df.query("question_no == 40")
-    winsorisation_df = winsorisation_df.query(
-        "{} == {} and question_no == 40".format(winsorisation_period, selected_period)
-    )
-
-    turnover_df = (
-        cp_df.merge(qv_df, how="left", on="reference")
-        .merge(finalsel_df, how="left", left_on="reference", right_on="ruref")
-        .merge(winsorisation_df, how="left", on="reference")
+    turnover_df = additional_outputs_df.query(
+        "period == {} and questioncode == 40".format(current_period)
     )
 
     turnover_df["curr_grossed_value"] = (
-        turnover_df["adjusted_value"]
+        turnover_df["adjustedresponse"]
         * turnover_df["design_weight"]
         * turnover_df["outlier_weight"]
         * turnover_df["calibration_factor"]
@@ -58,9 +37,11 @@ def create_turnover_output(
     # Convert imp_marker to type
     # Type 1: Return, Type 2: Construction, Type 3: Imputation
     type_conditions = [
-        turnover_df["imputation_marker"] == "r",
-        turnover_df["imputation_marker"].isin(["c", "mc"]),
-        turnover_df["imputation_marker"].isin(["fir", "bir", "fic", "fimc"]),
+        turnover_df["imputation_flags_adjustedresponse"] == "r",
+        turnover_df["imputation_flags_adjustedresponse"].isin(["c", "mc"]),
+        turnover_df["imputation_flags_adjustedresponse"].isin(
+            ["fir", "bir", "fic", "fimc"]
+        ),
     ]
 
     type_values = [1, 2, 3]
@@ -74,19 +55,19 @@ def create_turnover_output(
 
     turnover_df = turnover_df[
         [
-            "sic92",
+            "frosic2007",
             "cell_no",
             "reference",
-            "entname1",
-            "adjusted_value",
+            "referencename",
+            "adjustedresponse",
             "type",
             "curr_grossed_value",
             "outlier_weight",
-            "error_mkr",
+            "status",
             "error_res_code",
             "frotover",
             "froempment",
-            "returned_value",
+            "response",
         ]
     ]
 
