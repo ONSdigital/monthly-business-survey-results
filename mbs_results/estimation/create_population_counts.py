e345206434cb6d9d3271fab48b9c8527542b246d
Jordan Day <jordan.day@ons.gov.uk>
2024-12-17 10:37:44 +0000

    Create unit test, remove script entry point

--- mbs_results/estimation/create_population_counts.py
+++ mbs_results/estimation/create_population_counts.py
@@ -27,25 +27,8 @@ def create_population_count_output(
         df, period, strata, colname="population", **config
     )
 
-    # Check sampled == 1 is the correct way to filter on sampled
-    # if census, then not sampled change column column for "is census",
-    # change winsorisation
     df_2 = calculate_turnover_sum_count(
-        df.loc[df["sampled"] is True], period, strata, colname="sample", **config
+        df.loc[df["sampled"]], period, strata, colname="sample", **config
     )
     combined = pd.merge(df_1, df_2, on=[period, strata])
     return combined
-
-
-if __name__ == "__main__":
-    from mbs_results.utilities.inputs import load_config
-
-    config = load_config()
-    pop_file_path = "../population_counts_input.csv"
-    df = pd.read_csv(pop_file_path)
-    output = create_population_count_output(df, **config)
-    print(output)
-
-    print("\n Expected output:")
-    expected_output = pd.read_csv("../population_counts_output.csv")
-    print(expected_output)
---
+++ tests/estimation/test_create_population_counts.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+from mbs_results.estimation.create_population_counts import (
+    calculate_turnover_sum_count,
+    create_population_count_output,
+)
+
+
+def test_calculate_turnover_sum_count():
+    # Creating input data
+    df = pd.DataFrame(
+        {
+            "frotover": [1, 2, 3, 4, 5],
+            "reference": [1, 1, 2, 2, 3],
+            "period": [1, 1, 1, 1, 1],
+            "strata": ["A", "A", "B", "B", "C"],
+        }
+    )
+
+    # producing output
+    output = calculate_turnover_sum_count(df, "period", "strata", "population")
+
+    # creating expected output
+    expected_output = pd.DataFrame(
+        {
+            "period": [1, 1, 1],
+            "strata": ["A", "B", "C"],
+            "population_turnover_sum": [3, 7, 5],
+            "population_count": [2, 2, 1],
+        }
+    )
+    pd.testing.assert_frame_equal(output, expected_output)
+
+
+def test_create_population_count_output():
+    # Creating input data
+    df = pd.DataFrame(
+        {
+            "frotover": [1, 2, 3, 4, 5],
+            "reference": [1, 1, 2, 2, 3],
+            "period": [1, 1, 1, 1, 1],
+            "strata": ["A", "A", "B", "B", "C"],
+            "sampled": [True, False, True, False, True],
+        }
+    )
+
+    # producing output
+    output = create_population_count_output(df, "period", "strata")
+
+    # creating expected output
+    expected_output = pd.DataFrame(
+        {
+            "period": [1, 1, 1],
+            "strata": ["A", "B", "C"],
+            "population_turnover_sum": [3, 7, 5],
+            "population_count": [2, 2, 1],
+            "sample_turnover_sum": [1, 3, 5],
+            "sample_count": [1, 1, 1],
+        }
+    )
+    pd.testing.assert_frame_equal(output, expected_output)
