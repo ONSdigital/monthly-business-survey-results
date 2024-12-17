5792631e8dfe61f7ce3d4983459707c9734b1197
Jordan Day <jordan.day@ons.gov.uk>
2024-12-17 10:47:03 +0000

    Adding docstrings

--- mbs_results/estimation/create_population_counts.py
+++ mbs_results/estimation/create_population_counts.py
@@ -2,8 +2,28 @@ import pandas as pd
 
 
 def calculate_turnover_sum_count(
-    df: pd.DataFrame, period, strata, colname, **config
+    df: pd.DataFrame, period: str, strata: str, colname: str, **config
 ) -> pd.DataFrame:
+    """
+    Calculates turnover sum and count and returns an aggregated dataframe
+    with the given column name prefixed to the sum and count columns
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        original dataframe containing frotover. Groups by period and strata
+    period : str
+        period column name
+    strata : str
+        strate column name
+    colname : str
+        column name to prefix to the sum and count columns
+
+    Returns
+    -------
+    pd.DataFrame
+        A grouped dataframe with the sum and count columns prefixed with colname
+    """
 
     df_pop_count = (
         df.groupby([period, strata])
@@ -22,6 +42,24 @@ def calculate_turnover_sum_count(
 def create_population_count_output(
     df: pd.DataFrame, period, strata, **config
 ) -> pd.DataFrame:
+    """
+    creates the population count output
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        original dataframe frotover and sampled. Groups by period and strata
+    period : str
+        period column name
+    strata : str
+        strata column name
+
+    Returns
+    -------
+    pd.DataFrame
+        A grouped dataframe with the sum and count columns prefixed with colname.
+        Contains both population and sampled sum and counts for output.
+    """
 
     df_1 = calculate_turnover_sum_count(
         df, period, strata, colname="population", **config
