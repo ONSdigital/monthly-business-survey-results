0999b5202ee1d338abf9dbc9b101ff386efca8ec
Nathan Kelly <Nathan.Kelly@ons.gov.uk>
2024-12-18 12:17:25 +0000

    Add function and unit test for creating srs_ocea_outputs

---
+++ mbs_results/outputs/ocea_srs_outputs.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+
+def produce_ocea_srs_outputs(df: pd.DataFrame) -> pd.DataFrame:
+    """Produces outputs for OCEA/SRS
+
+    Parameters
+      ----------
+      df
+        Dataframe containing MBS data
+
+      Returns
+      -------
+      output_df
+          MBS output formatted according to SRS/OCEA requirements
+    """
+
+    output_df = df[
+        [
+            "reference",
+            "period",
+            "frosic2007",
+            "response",
+            "adjustedresponse",
+            "outlier_weight",
+            "calibration_factor",
+            "design_weight",
+            "frotover",
+            "froempment",
+        ]
+    ]
+
+    output_df = output_df.rename(
+        {
+            "reference": "Ruref",
+            "period": "Period",
+            "frosic2007": "SIC",
+            "response": "return",
+            "adjustedresponse": "adjusted",
+            "calibration_factor": "gweight",
+            "design_weight": "a-weight",
+            "frotover": "registered_turnover",
+            "froempment": "registered_employment",
+        },
+        axis=1,
+    )
+
+    return output_df
