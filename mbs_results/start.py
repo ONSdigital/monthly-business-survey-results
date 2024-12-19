038f09f6616f9315d1b50a8c61b9259fb54e6659
soderc <christoffer.soderberg@ons.gov.uk>
2024-12-19 14:31:03 +0000

    Updated move_files() to require a parameter

--- mbs_results/start.py
+++ mbs_results/start.py
@@ -1,14 +1,14 @@
 import os
 import shutil
 
-def move_files():
+def move_files(mbs_results_install_location):
     """
     Copy mbs_results/main.py and mbs_results/config.json from
     virtual environment site packages into the current working directory
     """
 
     # Get the directory where mbs_results is installed
-    target_path = os.path.dirname(mbs_results.__file__)
+    target_path = os.path.dirname(mbs_results_install_location)
 
     # Append main.py and config.json
     main_path = os.path.join(target_path, "main.py")
