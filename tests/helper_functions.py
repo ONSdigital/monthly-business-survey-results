29e2bfdd33e68c0595840561f130a8d0f19ea2ee
Derrick Njobuenwu <Derrick.Njobuenwu@ons.gov.uk>
2025-03-05 18:35:24 +0000

    fix formatting issues identified by pre-commit

--- tests/helper_functions.py
+++ tests/helper_functions.py
@@ -35,16 +35,16 @@ def does_not_raise():
 
 def create_testing_config(file_paths):
     """Copy config to testing directory and update file_paths"""
-    
+
     # merger the two config files into config.json
     # Load the user config
     with open("mbs_results/config_user.json", "r") as f:
-        user_config  = json.load(f)
-        
+        user_config = json.load(f)
+
     # Load the constants config
     with open("mbs_results/config_constants.json", "r") as f:
         constant_config = json.load(f)
-        
+
     # Merge both config files
     config = {**user_config, **constant_config}
 
