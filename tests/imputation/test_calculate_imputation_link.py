4546af04e0380dc4bdf7eec15476b40d209c6720
Anton Zogkolli <110612763+AntonZogk@users.noreply.github.com>
2025-03-07 14:35:40 +0000

    809 default imputation link (#181)
    * Add no matches scenario
    
    * Add default bool column
    
    * Remove count function from calculate link
    
    * Add counts in column of input df
    
    * Add count wrapper, update test count col name

--- tests/imputation/test_calculate_imputation_link.py
+++ tests/imputation/test_calculate_imputation_link.py
@@ -60,8 +60,5 @@ class TestLinks:
             predictive,
             link,
         )
-        df_input.drop(
-            columns=[match + "_pair_count", "default_link_" + match], inplace=True
-        )
 
         assert_frame_equal(df_input, df_output, check_like=True)
