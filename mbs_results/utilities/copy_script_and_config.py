b528926479850301d38d4faa062913475a2cf27f
Derrick Njobuenwu <Derrick.Njobuenwu@ons.gov.uk>
2025-03-10 14:39:27 +0000

    apply pre-commit fixes

--- mbs_results/utilities/copy_script_and_config.py
+++ mbs_results/utilities/copy_script_and_config.py
@@ -1,10 +1,10 @@
+import json
 import os
 import shutil
-import json
 
 import mbs_results
-from mbs_results.utilities.merge_two_config_files import merge_two_config_files
 from mbs_results import logger
+from mbs_results.utilities.merge_two_config_files import merge_two_config_files
 
 
 def copy_script_and_config():
@@ -31,13 +31,15 @@ def copy_script_and_config():
     # Append main.py and config.json
     config_user_path = os.path.join(target_path, "config_user.json")
     config_dev_path = os.path.join(target_path, "config_dev.json")
-    
+
     # Merge the config_user.json and config_dev.json
     # into config.json in the mbs_results dir
     config = merge_two_config_files(config_user_path, config_dev_path)
-    logger.info(f"{config_path} created from merging {config_user_path} and {config_dev_path}")
-    
-    # Dump the config.json  
+    logger.info(
+        f"{config_path} created from merging {config_user_path} and {config_dev_path}"
+    )
+
+    # Dump the config.json
     with open(config_path, "w") as f:
         json.dump(config, f)
 
@@ -47,7 +49,7 @@ def copy_script_and_config():
     # Copy the files
     shutil.copy(main_path, working_directory)
     shutil.copy(config_path, working_directory)
-    
-    # remove the config.json. 
+
+    # remove the config.json.
     # Note that config. json = concatenation of config_user.json and config_dev.json
     os.remove(config_path)
