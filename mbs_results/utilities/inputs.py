e3bee57060cbe6ca8b9f7734703eaae12d17427c
Derrick Njobuenwu <Derrick.Njobuenwu@ons.gov.uk>
2025-03-10 13:24:28 +0000

    update files and code for review config

--- mbs_results/utilities/inputs.py
+++ mbs_results/utilities/inputs.py
@@ -1,9 +1,16 @@
 import json
+from mbs_results import logger
 
 
 def load_config(path="config.json"):
-    with open(path, "r") as f:
-        config = json.load(f)
+    try:
+        with open(path, "r") as f:
+            config = json.load(f)
+        logger.info(f'Successfully loaded the config file from path: {path}')
+    except FileNotFoundError as e:
+        logger.error(f"if {path} is not found, run the setup_mbs to create {path} from merging the two JSON files")
+        raise e
+    
     config["mbs_results_path"] = config["folder_path"] + config["mbs_file_name"]
-
+    
     return config
--- mbs_results/utilities/merge_two_config_files.py
+++ mbs_results/utilities/merge_two_config_files.py
@@ -2,38 +2,31 @@ import json
 
 
 def merge_two_config_files(
-    user_config_path="config_user.json",
-    constants_config_path="config_constants.json",
-    destination_path=None,
+    config_user_path="mbs_results/config_user.json",
+    config_dev_path="mbs_results/config_dev.json"
 ):
     """
     Load and merge two configuration files: a user-specific config and a
-    constants config.
+    dev-specific config.
 
     Parameters
     ----------
-    user_config_path : str, optional
-        Path to the user-specific configuration file (default is "config_user.json").
-    constants_config_path : str, optional
-        Path to the constants configuration file (default is "config_constants.json")..
-
+    config_user_path : str, optional
+        Path to the user-specific configuration file (default is "mbs_results/config_user.json").
+    config_dev_path : str, optional
+        Path to the dev-specific configuration file (default is "mbs_results/config_dev.json").
+    
     Returns
     -------
-    Dict
+    config : Dict
         A dictionary containing the merged configuration.
     """
 
-    # Load the user config
-    with open(user_config_path, "r") as f:
-        user_config = json.load(f)
+    with open(config_user_path, "r") as f:
+        config_user = json.load(f)
 
-    # Load the constants config
-    with open(constants_config_path, "r") as f:
-        constant_config = json.load(f)
+    with open(config_dev_path, "r") as f:
+        config_dev = json.load(f)
 
-    # Merge both config files
-    config = {**user_config, **constant_config}
-
-    # Dump the config.json in the destination_path
-    with open(destination_path, "w") as f:
-        json.dump(config, f)
+    config = {**config_user, **config_dev}
+    return config
