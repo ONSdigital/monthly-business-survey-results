0c8fd2836ca593e3df439192c55e26db30336a5f
soderc <christoffer.soderberg@ons.gov.uk>
2025-03-21 12:35:40 +0000

    Added unit test and associated test data for replace_outlier_weights

--- tests/utilities/test_constrains.py
+++ tests/utilities/test_constrains.py
@@ -10,6 +10,7 @@ from mbs_results.utilities.constrains import (
     replace_values_index_based,
     sum_sub_df,
     update_derived_weight_and_winsorised_value,
+    replace_outlier_weights
 )
 
 
@@ -239,6 +240,28 @@ def test_update_derived_weight_and_winsorised_value(filepath, base_file_name):
         "spp_form_id",
         "outlier_weight",
         "value",
+    )
+
+    assert_frame_equal(df_actual, df_expected)
+
+def test_replace_outlier_weights(filepath):
+
+    df = pd.read_csv(
+        filepath / "test_replace_outliers.csv",
+        index_col=False
+    )
+
+    df_in = df.drop(columns=["manual_outlier_weight"])
+
+    df_expected = df.drop(columns=["outlier_weight"])
+
+    df_actual = replace_outlier_weights(
+        df_in,
+        "reference",
+        "period",
+        "question_no",
+        "outlier_weight",
+        filepath / "test_manual_outliers_file.csv"
     )
 
     assert_frame_equal(df_actual, df_expected)
