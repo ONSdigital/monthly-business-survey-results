4ba24a7aed51f20e72037869afef5b95e6ad2912
Nathan Kelly <Nathan.Kelly@ons.gov.uk>
2025-04-30 16:37:50 +0000

    Fix issue with drop_derived_questions

--- tests/staging/test_stage_dataframe.py
+++ tests/staging/test_stage_dataframe.py
@@ -13,33 +13,41 @@ def filepath():
 
 
 def test_drop_derived_questions(filepath):
-    df_input = pd.read_csv(filepath / "drop_derived_questions_input.csv")
+    df_input = pd.read_csv(
+        filepath / "drop_derived_questions_input.csv", dtype={"form_type": "str"}
+    )
 
     test_config = {
         "form_to_derived_map": {
-            13: [40],
-            14: [40],
-            15: [46],
-            16: [42],
+            "13": [40],
+            "14": [40],
+            "15": [46],
+            "16": [42],
         }
     }
 
     actual_output = drop_derived_questions(
         df_input, "question_no", "form_type", test_config["form_to_derived_map"]
     )
-    expected_output = pd.read_csv(filepath / "drop_derived_questions_output.csv")
+    expected_output = pd.read_csv(
+        filepath / "drop_derived_questions_output.csv", dtype={"form_type": "str"}
+    )
 
     assert_frame_equal(actual_output, expected_output)
 
 
 def test_drop_derived_questions_multiple(filepath):
-    df_input = pd.read_csv(filepath / "drop_derived_questions_multi_input.csv")
+    df_input = pd.read_csv(
+        filepath / "drop_derived_questions_multi_input.csv", dtype={"form_type": "str"}
+    )
 
-    test_config = {"form_to_derived_map": {13: [40, 46, 42, 40]}}
+    test_config = {"form_to_derived_map": {"13": [40, 46, 42, 40]}}
 
     actual_output = drop_derived_questions(
         df_input, "question_no", "form_type", test_config["form_to_derived_map"]
     )
-    expected_output = pd.read_csv(filepath / "drop_derived_questions_multi_output.csv")
+    expected_output = pd.read_csv(
+        filepath / "drop_derived_questions_multi_output.csv", dtype={"form_type": "str"}
+    )
 
     assert_frame_equal(actual_output, expected_output)
