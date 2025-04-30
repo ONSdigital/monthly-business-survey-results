d38516c41f1e21516a485d624c6cfa22be248141
Nathan Kelly <Nathan.Kelly@ons.gov.uk>
2025-04-30 16:20:44 +0000

    Add in additional test case

--- tests/staging/test_stage_dataframe.py
+++ tests/staging/test_stage_dataframe.py
@@ -30,3 +30,16 @@ def test_drop_derived_questions(filepath):
     expected_output = pd.read_csv(filepath / "drop_derived_questions_output.csv")
 
     assert_frame_equal(actual_output, expected_output)
+
+
+def test_drop_derived_questions_multiple(filepath):
+    df_input = pd.read_csv(filepath / "drop_derived_questions_multi_input.csv")
+
+    test_config = {"form_to_derived_map": {13: [40, 46, 42, 40]}}
+
+    actual_output = drop_derived_questions(
+        df_input, "question_no", "form_type", test_config["form_to_derived_map"]
+    )
+    expected_output = pd.read_csv(filepath / "drop_derived_questions_multi_output.csv")
+
+    assert_frame_equal(actual_output, expected_output)
