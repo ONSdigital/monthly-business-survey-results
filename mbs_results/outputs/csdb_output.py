caa4421dca9de17f1f522d0a3c0696f7454321ab
dekorte-ons <ellen.de.korte@ons.gov.uk>
2025-06-04 12:27:34 +0100

    Add strip() function to get rid of leading or trailing white spaces

--- mbs_results/outputs/csdb_output.py
+++ mbs_results/outputs/csdb_output.py
@@ -35,7 +35,7 @@ def create_csdb_output(
     df_combined["period"] = convert_column_to_datetime(
         df_combined["period"]
     ).dt.strftime("%Y%m")
-    df_combined["cdid"] = df_combined["cdid"].strip()
+    df_combined["cdid"] = df_combined.replace()["cdid"].apply(lambda x: x.strip() if isinstance(x, str) else x)
     # Convert grossed_column into pounds thousands before agg
     df_combined["curr_grossed_value"] = (
         df_combined["adjustedresponse"]
