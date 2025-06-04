3c092fa777e167eee5374513658d3e47ae7f7728
dekorte-ons <ellen.de.korte@ons.gov.uk>
2025-06-04 11:55:00 +0100

    Change file names unit test script

--- tests/outputs/test_csdb_output.py
+++ tests/outputs/test_csdb_output.py
@@ -14,12 +14,12 @@ def filepath():
 
 @pytest.fixture(scope="class")
 def input_df(filepath):
-    return pd.read_csv(filepath / "input_df.csv", index_col=False)
+    return pd.read_csv(filepath / "input_df_2.csv", index_col=False)
 
 
 @pytest.fixture(scope="class")
 def output_df(filepath):
-    return pd.read_csv(filepath / "output_df.csv", index_col=False)
+    return pd.read_csv(filepath / "output_df_2.csv", index_col=False)
 
 
 class TestCSDBOutput:
@@ -33,7 +33,7 @@ class TestCSDBOutput:
 
         actual_output = create_csdb_output(
             additional_outputs_df=input_df,
-            cdid_data_path=Path(filepath / "cdid_mapping.csv"),
+            cdid_data_path=Path(filepath / "cdid_mapping_2.csv"),
         )
 
         assert_frame_equal(actual_output, expected_output)
