9656343e851ccce0744ebceaf7a8b8cee7900dc2
NathanKelly-ONS <93586797+NathanKelly-ONS@users.noreply.github.com>
2025-06-23 08:45:50 +0100

    969 shadow team output (#247)
    * Create QA outputs and unit tests
    
    * Add produce_qa_output to main and update tests
    
    * Troubleshooting why integration test is failing
    
    * Update qa_output function and tests with correct col names
    
    * Update unit tests
    
    * Update so pre-commit hooks pass

---
+++ mbs_results/outputs/qa_output.py
@@ -0,0 +1,75 @@
+import pandas as pd
+
+
+def produce_qa_output(config: dict, post_win_df: pd.DataFrame) -> pd.DataFrame:
+    """Produces an output with required columns, and with total weight and
+    weighted adjusted value calculated.
+
+     Parameters
+    ----------
+    config : dict
+        The config as a dictionary.
+    post_win_df : pd.DataFrame
+        Dataframe containing the required columns, following the
+        outlier_detection module.
+    """
+
+    target = config["target"]
+
+    requested_columns = [
+        "reference",
+        "period",
+        "sic",
+        "classification",
+        "cell_number",
+        "auxiliary",
+        "froempment",
+        "formtype",
+        "imputed_and_derived_flag",  # response_type
+        "question_no",
+        "target",
+        "status",  # error_mkr
+        "design_weight",
+        "calibration_factor",
+        "outlier_weight",
+        "total weight (A*G*O)",
+        "weighted adjusted value",
+        f"imputation_flags_{target}",
+        "imputation_class",
+        f"f_link_{target}",
+        f"default_link_f_match_{target}",
+        f"b_link_{target}",
+        f"default_link_b_match_{target}",
+        "construction_link",
+        "flag_construction_matches_count",
+        "default_link_flag_construction_matches",
+        "constrain_marker",  # these not requested but useful
+    ]
+
+    if not config["filter"]:
+        count_variables = [f"b_match_{target}_count", f"f_match_{target}_count"]
+    else:
+        count_variables = [
+            f"b_match_filtered_{target}_count",
+            f"f_match_filtered_{target}_count",
+        ]
+
+    requested_columns += count_variables
+
+    # Check if column names specified in config, if not, use above as default
+    cols_from_config = []
+
+    for col in requested_columns:
+        cols_from_config.append(config.get(col, col))
+
+    post_win_df["total weight (A*G*O)"] = (
+        post_win_df[config["design_weight"]]
+        * post_win_df[config["calibration_factor"]]
+        * post_win_df["outlier_weight"]
+    )
+
+    post_win_df["weighted adjusted value"] = (
+        post_win_df[config["target"]] * post_win_df["total weight (A*G*O)"]
+    )
+
+    return post_win_df[cols_from_config]
