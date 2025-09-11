48ef16201592264348c8fe82fbd2d6401a46f3a9
soderc <christoffer.soderberg@ons.gov.uk>
2025-09-11 15:32:05 +0100

    basic formatting changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,29 +9,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'monthly-business-survey-results'
-copyright = '2025, ONSdigital'
-author = 'ONSdigital'
-release = '0.1.13'
+project = "monthly-business-survey-results"
+copyright = "2025, ONSdigital"
+author = "ONSdigital"
+release = "0.1.13"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sys
-from pathlib import Path
 sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 extensions = [
-    'myst_parser',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'numpydoc',
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "numpydoc",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
 
 autodoc_default_options = {
@@ -37,16 +38,16 @@ autodoc_default_options = {
     "private-members": True,
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-# todo: autosummary combined with myst_parser causes crash when converting project_docs_md
+# TODO: autosummary and myst_parser causes crash when converting project_docs_md
 exclude_patterns = [
-    'project_docs_md',
-] 
+    "project_docs_md",
+]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
