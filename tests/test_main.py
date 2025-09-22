1000e70ebee838adcbdf791f10d503ca088d107e
soderc <christoffer.soderberg@ons.gov.uk>
2025-09-22 11:37:57 +0100

    update test suite

--- tests/test_main.py
+++ tests/test_main.py
@@ -36,6 +36,8 @@ test_config = {
     "devolved_nations": ["Scotland", "Wales"],
     "optional_outputs": ["all"],
     "cdid_data_path": "tests/data/outputs/csdb_output/cdid_mapping.csv",
+    "generate_schemas": True,
+    "schema_path": "tests/data/test_main/schemas/",
 }
 
 
