aadd84d2c5bd39091f21583f23b5a1d20d6953e2
Matthews <Katey.Matthews@ons.gov.uk>
2025-10-08 12:00:36 +0100

    SIC, previous period and difference columns included. Filtered by period

--- mbs_results/estimation/create_population_counts.py
+++ mbs_results/estimation/create_population_counts.py
@@ -41,6 +41,7 @@ def calculate_turnover_sum_count(
     return df_pop_count
 
 
+
 def create_population_count_output(
     df: pd.DataFrame,
     period: str,
@@ -72,15 +73,56 @@ def create_population_count_output(
         Contains both population and sampled sum and counts for output.
         Returns none if save_output is True
     """
+    #KM REMOVE - FIND CODE FOR PREV_PERIOD  
+    current_period = int(config["current_period"])
+    previous_period = 202201
 
-    df_population = calculate_turnover_sum_count(
-        df, period, strata, colname="population", **config
-    )
+    print("unique periods in df:", df[period].unique())
+    print("current eriod:", current_period)
+    print("previous period:", previous_period)
+    print("rows matching prev period:", df[df[period] == previous_period].shape[0])
+    print("rows matching curremt period:", df[df[period] == current_period].shape[0])
+    
+    df_prev = df[df[period] == previous_period]
+    df_curr = df[df[period] == current_period]
 
-    df_sampled = calculate_turnover_sum_count(
-        df.loc[df["is_sampled"]], period, strata, colname="sample", **config
-    )
-    combined = pd.merge(df_population, df_sampled, on=[period, strata])
+    population_prev = calculate_turnover_sum_count(df_prev, period, strata, "population", **config)
+    population_curr = calculate_turnover_sum_count(df_curr, period, strata, "population", **config)
+
+    sample_prev = calculate_turnover_sum_count(df_prev[df_prev["is_sampled"]], period, strata, "sample", **config)
+    sample_curr = calculate_turnover_sum_count(df_curr[df_curr["is_sampled"]], period, strata, "sample", **config)
+
+    population_prev = population_prev.drop(columns=[period])
+    population_curr = population_curr.drop(columns=[period])
+    sample_prev = sample_prev.drop(columns=[period])
+    sample_curr = sample_curr.drop(columns=[period])
+
+    prev = pd.merge(population_prev, sample_prev, on=[strata], how="outer").fillna(0)
+    curr = pd.merge(population_curr, sample_curr, on=[strata], how="outer").fillna(0)
+
+    
+
+    prev = prev.rename(columns={
+        "population_turnover_sum": "population_turnover_previous",
+        "population_count": "population_count_previous",
+        "sample_turnover_sum": "sample_turnover_previous",
+        "sample_count": "sample_count_previous",
+    })
+
+    curr = curr.rename(columns={
+        "population_turnover_sum": "population_turnover_current",
+        "population_count": "population_count_current",
+        "sample_turnover_sum": "sample_turnover_current",
+        "sample_count": "sample_count_current",
+    })
+
+    combined = pd.merge(curr, prev, on=[strata], how="outer").fillna(0)
+
+    combined["population_turnover_difference"] = combined["population_turnover_current"] - combined["population_turnover_previous"]
+    combined["sample_turnover_difference"] = combined["sample_turnover_current"] - combined["sample_turnover_previous"]
+    combined["population_count_difference"] = combined["population_count_current"] - combined["population_count_previous"]
+    combined["sample_count_difference"] = combined["sample_count_current"] - combined["sample_count_previous"]
+
 
     if save_output:
         write_csv_wrapper(
