672608709bc64ee2a4d94c42c1ec33243d93bf3f
Jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-10-15 14:39:11 +0100

    Population counts feedback (#295)
    * SIC, previous period and difference columns included. Filtered by period
    
    * refactor output method so both versions are produced for mbs but only full is produced for cons
    
    * adding docstring and updating args
    
    * Creating unit test
    
    * remove outer join, fillna and print statements
    
    * updating merge to use left join instead of default inner join
    
    * changing to outer
    
    ---------
    
    Co-authored-by: Matthews <Katey.Matthews@ons.gov.uk>

--- mbs_results/estimation/create_population_counts.py
+++ mbs_results/estimation/create_population_counts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from mbs_results.utilities.inputs import read_csv_wrapper
 from mbs_results.utilities.outputs import write_csv_wrapper
 
 
@@ -45,8 +46,9 @@ def create_population_count_output(
     df: pd.DataFrame,
     period: str,
     strata: str,
-    output_path: str = "",
-    save_output: bool = False,
+    output_path: str,
+    platform: str,
+    bucket: str,
     **config: dict,
 ) -> pd.DataFrame:
     """
@@ -73,24 +75,116 @@ def create_population_count_output(
         Returns none if save_output is True
     """
 
-    df_population = calculate_turnover_sum_count(
-        df, period, strata, colname="population", **config
+    population = calculate_turnover_sum_count(
+        df, period, strata, "population", **config
+    )
+    sample = calculate_turnover_sum_count(
+        df[df["is_sampled"]], period, strata, "sample", **config
     )
 
-    df_sampled = calculate_turnover_sum_count(
-        df.loc[df["is_sampled"]], period, strata, colname="sample", **config
+    # outer join and filling na to keep cases when population is is non-zero,
+    # and nothing is in sample (i.e. cell has not been sampled)
+    full_combined = pd.merge(
+        population, sample, on=[period, strata], how="outer"
+    ).fillna(0)
+
+    write_csv_wrapper(
+        full_combined,
+        output_path + "population_counts.csv",
+        platform,
+        bucket,
+        index=False,
     )
-    combined = pd.merge(df_population, df_sampled, on=[period, strata])
 
-    if save_output:
-        write_csv_wrapper(
-            combined,
-            output_path + "population_counts.csv",
-            config["platform"],
-            config["bucket"],
-            index=False,
-        )
+    return full_combined
 
-        return
-    else:
-        return combined
+
+def format_population_counts_mbs(
+    period: str,
+    strata: str,
+    output_path: str,
+    platform: str,
+    bucket: str,
+    current_period: str,
+    **config: dict,
+):
+    """
+    produces the formatted population counts for mbs pipeline.
+    Loads the full
+
+    Parameters
+    ----------
+    period : str
+        period column name
+    strata : str
+        strata column name
+    output_path : str
+        output path where population_counts.csv has been stored
+    platform : str
+        platform name, either "s3" or "network"
+    bucket : str
+        bucket name when loading from "s3"
+    current_period : str
+        current period in "YYYYMM" format
+
+    Returns
+    -------
+    tuple of (pd.DataFrame, str)
+        returns the formatted dataframe and the filename including current and previous
+        period in filename
+    """
+
+    df = read_csv_wrapper(
+        filepath=output_path + "population_counts.csv",
+        import_platform=platform,
+        bucket_name=bucket,
+    )
+
+    # Better way to do this?
+    current_period = pd.to_datetime(current_period, format="%Y%m")
+    previous_period = int((current_period - pd.DateOffset(months=1)).strftime("%Y%m"))
+    current_period = int(current_period.strftime("%Y%m"))
+
+    df_prev = df[df[period] == previous_period]
+    df_curr = df[df[period] == current_period]
+
+    df_prev = df_prev.drop(columns=[period])
+    df_curr = df_curr.drop(columns=[period])
+
+    prev = df_prev.rename(
+        columns={
+            "population_turnover_sum": "population_turnover_sum_previous",
+            "population_count": "population_count_previous",
+            "sample_turnover_sum": "sample_turnover_sum_previous",
+            "sample_count": "sample_count_previous",
+        }
+    )
+
+    curr = df_curr.rename(
+        columns={
+            "population_turnover_sum": "population_turnover_sum_current",
+            "population_count": "population_count_current",
+            "sample_turnover_sum": "sample_turnover_sum_current",
+            "sample_count": "sample_count_current",
+        }
+    )
+
+    combined = pd.merge(curr, prev, on=strata, how="outer").fillna(0)
+
+    combined["population_turnover_difference"] = (
+        combined["population_turnover_sum_current"]
+        - combined["population_turnover_sum_previous"]
+    )
+    combined["sample_turnover_difference"] = (
+        combined["sample_turnover_sum_current"]
+        - combined["sample_turnover_sum_previous"]
+    )
+    combined["population_count_difference"] = (
+        combined["population_count_current"] - combined["population_count_previous"]
+    )
+    combined["sample_count_difference"] = (
+        combined["sample_count_current"] - combined["sample_count_previous"]
+    )
+
+    filename = f"mbs_population_counts_{current_period}_{previous_period}.csv"
+    return (combined, filename)
