420b40f002b45ea14fb67c9b68f0cfa41de2ddfb
Nathan Kelly <nathan.kelly@ons.gov.uk>
2025-10-22 12:40:11 +0100

    Update non_response in responses to output file and add in check_for_null_target

--- mbs_results/staging/validate_snapshot.py
+++ mbs_results/staging/validate_snapshot.py
@@ -2,14 +2,84 @@ import logging
 
 import pandas as pd
 
+from mbs_results.utilities.outputs import save_df
 
-def validate_snapshot(
+
+def check_for_null_target(
+    config: dict,
+    responses: pd.DataFrame,
+    target: str,
+    question_no: str,
+) -> None:
+    """
+    Validates that there are no empty strings and nulls in the target column of
+    responses.
+
+    Parameters
+    ----------
+    config : dict
+        Dictionary containing "filter_out_questions" key.
+    responses : pd.DataFrame
+        DataFrame containing responses data.
+    target : str
+        The name of the target column.
+    question_no : str
+        The name of the question number.
+
+    Raises
+    ------
+    Warning
+        If any empty strings or nulls are found in the target column, a warning
+        is logged with details.
+    """
+    logger = logging.getLogger(__name__)
+
+    # Check for empty strings in the target column
+    empty_string_rows = responses[responses[target] == ""]
+    if not empty_string_rows.empty:
+        if {"reference", "period"}.issubset(empty_string_rows.columns):
+            first_5 = (
+                empty_string_rows[["reference", "period"]]
+                .head(5)
+                .drop_duplicates()
+                .values.tolist()
+            )
+
+        logger.warning(
+            f"There are {len(empty_string_rows)} rows with empty strings in the "
+            f"'{target}' column. The first 5 (or less) (reference, period) groups are: "
+            f"{first_5}"
+        )
+
+    filter_out_qs = config["filter_out_questions"]
+    filtered_responses = responses[~responses[question_no].isin(filter_out_qs)]
+
+    # Check for nulls in the target column
+    null_rows = filtered_responses[filtered_responses[target].isnull()]
+    if not null_rows.empty:
+        if {"reference", "period"}.issubset(null_rows.columns):
+            first_5 = (
+                null_rows[["reference", "period"]]
+                .head(5)
+                .drop_duplicates()
+                .values.tolist()
+            )
+
+        logger.warning(
+            f"There are {len(null_rows)} rows with nulls in the '{target}' column "
+            f"after filtering out questions in {filter_out_qs}.  "
+            f"The first 5 (or less) (reference, period) groups are: {first_5}"
+        )
+
+
+def non_response_in_responses(
     responses: pd.DataFrame,
     contributors: pd.DataFrame,
     status: str,
     reference: str,
     period: str,
     non_response_statuses: list,
+    config: dict,
 ):
     """
     Validate that there are no reference and period groupings that are listed as
@@ -30,13 +100,16 @@ def validate_snapshot(
     non_response_statuses : list
         A list of statuses that should be treated as non-responders. These
         should be present in the `status` column of the contributors dataframe.
+    config : dict
+        The config read in as a dictionary.
 
 
     Raises
     ------
     Warning
-        If any reference and period groupings are found in both non-response statuses
-        in contributors and in responses, a warning is raised with details.
+        If any reference and period groupings are found in both non-response
+        statuses in contributors and in responses, a warning is raised with
+        details.
     """
 
     logger = logging.getLogger(__name__)
@@ -48,16 +121,30 @@ def validate_snapshot(
         non_responses = non_responses.set_index([reference, period])
         responses = responses.set_index([reference, period])
 
-        non_response_in_responses = responses.index.intersection(non_responses.index)
+        non_responses.to_csv("debug_non_responses.csv")
+        responses.to_csv("debug_responses.csv")
 
-        if len(non_response_in_responses) > 0:
-            warning_message = f"""There are {len(non_response_in_responses)} period and
+        common_index = responses.index.intersection(non_responses.index)
+
+        if len(common_index) > 0:
+            warning_message = f"""There are {len(common_index)} period and
             reference groupings that are listed as non-response statuses in contributors
             but are present in responses. The first 5 (or less) of these are:
-        {non_response_in_responses[:min(5, len(non_response_in_responses))].to_list()}.
+        {common_index[:min(5, len(common_index))].to_list()}.
         The target value for these reference and periods will be set to null."""  # noqa
 
             logger.warning(warning_message)
+
+            non_response_in_responses = responses.loc[common_index]
+
+            save_df(
+                non_response_in_responses.reset_index(),
+                "non_response_in_responses.csv",
+                config,
+                config["debug_mode"],
+            )
+
+            return non_response_in_responses
 
     else:
         logger.warning(
--- tests/staging/test_validate_snapshot.py
+++ tests/staging/test_validate_snapshot.py
@@ -4,7 +4,53 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from mbs_results.staging.validate_snapshot import validate_snapshot
+from mbs_results.staging.validate_snapshot import (
+    check_for_null_target,
+    non_response_in_responses,
+)
+
+
+@pytest.fixture(scope="class")
+def check_for_null_target_responses(filepath):
+    return pd.read_csv(
+        filepath / "check_for_null_target_responses.csv", index_col=False
+    )
+
+
+@pytest.fixture(scope="class")
+def config():
+    return {
+        "filter_out_questions": [11, 12],
+        "debug_mode": False,
+    }
+
+
+class TestCheckForNullTarget:
+    def test_check_for_null_target_nulls(
+        self, caplog, check_for_null_target_responses, config
+    ):
+        with caplog.at_level(logging.WARN):
+            check_for_null_target(
+                config=config,
+                responses=check_for_null_target_responses,
+                target="target",
+                question_no="questioncode",
+            )
+        assert "nulls" in caplog.text
+
+    def test_check_for_null_target_empty_strings(
+        self, caplog, check_for_null_target_responses, config
+    ):
+        responses = check_for_null_target_responses.copy()
+        responses["target"] = responses["target"].fillna("")
+        with caplog.at_level(logging.WARN):
+            check_for_null_target(
+                config=config,
+                responses=responses,
+                target="target",
+                question_no="questioncode",
+            )
+        assert "empty strings" in caplog.text
 
 
 @pytest.fixture(scope="class")
@@ -26,23 +72,59 @@ def responses_input(filepath):
     )
 
 
-class TestValidateSnapshot:
-    def test_validate_snapshot(
+@pytest.fixture(scope="class")
+def expected_non_response_in_responses(filepath):
+    return pd.read_csv(
+        filepath.parent
+        / "validate_snapshot"
+        / "non_response_in_responses_expected_output.csv",
+        index_col=False,
+    )
+
+
+class TestNonResponseInResponses:
+    def test_non_response_in_responses_output(
+        self,
+        contributors,
+        responses_input,
+        expected_non_response_in_responses,
+        config,
+    ):
+        result_df = non_response_in_responses(
+            responses=responses_input,
+            contributors=contributors,
+            status="status",
+            reference="reference",
+            period="period",
+            non_response_statuses=["Form sent out", "Excluded from results"],
+            config=config,
+        )
+        result_df = result_df.reset_index()
+
+        result_df.to_csv("debug_result_df.csv", index=False)
+
+        pd.testing.assert_frame_equal(
+            result_df,
+            expected_non_response_in_responses,
+        )
+
+    def test_non_response_in_responses(
         self,
         caplog,
         contributors,
         responses_input,
+        config,
     ):
         with caplog.at_level(logging.WARN):
-            validate_snapshot(
+            non_response_in_responses(
                 responses=responses_input,
                 contributors=contributors,
                 status="status",
                 reference="reference",
                 period="period",
                 non_response_statuses=["Form sent out", "Excluded from results"],
+                config=config,
             )
-
             assert (
                 """There are 2 period and
             reference groupings that are listed as non-response statuses in contributors
